16aa424a4dff728a1d9ea8e84466f4b9d0149b31
Greg Price <greg@zulipchat.com>
2018-09-16 09:46:34 -0700

    wip move attempt for GlobalState and members; needs fixup
    First WebStorm, then reverted most of that in favor of re-export.

---
+++ src/stateTypes.js
@@ -0,0 +1,202 @@
+/**
+ * Our complete Redux state tree.
+ *
+ * Each property is a subtree maintained by its own reducer function.
+ */
+/**
+ * The `presence` subtree of our Redux state.
+ *
+ * @prop (email) - Indexes over all users for which the app has received a
+ *   presence status.
+ */
+/**
+ * A collection of (almost) all users in the Zulip org; our `users` state subtree.
+ *
+ * This contains all users except deactivated users and cross-realm bots.
+ * For those, see RealmState.
+ */
+/**
+ * State with general info about a Zulip organization; our state subtree `realm`.
+ *
+ * @prop twentyFourHourTime
+ * @prop canCreateStreams
+ * @prop crossRealmBots - The server's cross-realm bots; e.g., Welcome Bot.
+ *   Cross-realm bots should be treated like normal bots.
+ * @prop nonActiveUsers - All users in the organization with `is_active`
+ *   false; for normal users, this means they or an admin deactivated their
+ *   account.  See `User` and the linked documentation.
+ * @prop pushToken
+ * @prop filters
+ * @prop emoji
+ * @prop isAdmin
+ */
+/**
+ * Info about how completely we know the messages in each narrow of
+ * MessagesState.
+ */
+/**
+ * A map with all messages we've stored locally, indexed by ID.
+ *
+ * See also `NarrowsState`, which is an index on this data that identifies
+ * messages belonging to a given narrow.
+ */
+/**
+ * An index on `MessagesState`, listing messages in each narrow.
+ *
+ * Keys are `JSON.stringify`-encoded `Narrow` objects.
+ * Values are sorted lists of message IDs.
+ *
+ * See also `MessagesState`, which stores the message data indexed by ID.
+ */
+import type { Message, RealmEmojiState, RealmFilter, Stream, Subscription, Topic, User } from './api/apiTypes';
+import type {
+  Account,
+  CaughtUp,
+  Debug,
+  Dimensions,
+  EditMessage,
+  Fetching,
+  MuteTuple,
+  Orientation,
+  Outbox,
+  Presence,
+  RealmBot,
+  ThemeType,
+  UnreadHuddlesState,
+  UnreadMentionsState,
+  UnreadPmsState,
+  UnreadStreamsState,
+  UserGroup
+} from './types';
+
+export type NarrowsState = {
+  [narrow: string]: number[],
+};
+export type MessagesState = {
+  [id: number]: Message,
+};
+export type StreamsState = Stream[];
+export type SubscriptionsState = Subscription[];
+export type AccountsState = Account[];
+export type SessionState = {
+  eventQueueId: number,
+  editMessage: ?EditMessage,
+  isOnline: boolean,
+  isActive: boolean,
+  isHydrated: boolean,
+  needsInitialFetch: boolean,
+  orientation: Orientation,
+  outboxSending: boolean,
+  safeAreaInsets: Dimensions,
+  debug: Debug,
+};
+export type CaughtUpState = {
+  [narrow: string]: CaughtUp,
+};
+export type FetchingState = {
+  [narrow: string]: Fetching,
+};
+export type FlagsState = {
+  [flagName: string]: {
+    [messageId: number]: boolean,
+  },
+};
+export type MigrationsState = {
+  version?: string,
+};
+export type LoadingState = {
+  presence: boolean,
+  subscriptions: boolean,
+  streams: boolean,
+  unread: boolean,
+  users: boolean,
+};
+export type MuteState = MuteTuple[];
+export type MuteState = MuteTuple[];
+export type NavigationState = {
+  index: number,
+  isTransitioning: boolean,
+  key: string,
+  routes: Array<{
+    key: string,
+    title: string,
+    routeName: string,
+    /** The fields in `params` vary by route; see `navActions.js`. */
+    params: {
+      narrow?: Narrow,
+    },
+  }>,
+};
+export type RealmState = {
+  twentyFourHourTime: boolean,
+  canCreateStreams: boolean,
+  crossRealmBots: RealmBot[],
+  nonActiveUsers: User[],
+  pushToken: {
+    token: string,
+    msg: string,
+    result: string,
+  },
+  filters: RealmFilter[],
+  emoji: RealmEmojiState,
+  isAdmin: boolean,
+};
+export type TopicsState = {
+  [number]: Topic[],
+};
+export type SettingsState = {
+  locale: string,
+  theme: ThemeType,
+  offlineNotification: boolean,
+  onlineNotification: boolean,
+  experimentalFeaturesEnabled: boolean,
+  streamNotification: boolean,
+};
+export type TypingState = {
+  [normalizedRecipients: string]: {
+    time: number,
+    userIds: number[],
+  },
+};
+export type AlertWordsState = string[];
+export type DraftsState = {
+  [narrow: string]: string,
+};
+export type UsersState = User[];
+export type UserGroupsState = UserGroup[];
+export type PresenceState = {
+  [email: string]: Presence,
+};
+export type OutboxState = Outbox[];
+export type UnreadState = {
+  streams: UnreadStreamsState,
+  huddles: UnreadHuddlesState,
+  pms: UnreadPmsState,
+  mentions: UnreadMentionsState,
+};
+export type GlobalState = {
+  accounts: AccountsState,
+  alertWords: AlertWordsState,
+  caughtUp: CaughtUpState,
+  drafts: DraftsState,
+  fetching: FetchingState,
+  flags: FlagsState,
+  migrations: MigrationsState,
+  loading: LoadingState,
+  messages: MessagesState,
+  mute: MuteState,
+  narrows: NarrowsState,
+  nav: NavigationState,
+  outbox: OutboxState,
+  presence: PresenceState,
+  realm: RealmState,
+  session: SessionState,
+  settings: SettingsState,
+  streams: StreamsState,
+  subscriptions: SubscriptionsState,
+  topics: TopicsState,
+  typing: TypingState,
+  unread: UnreadState,
+  userGroups: UserGroupsState,
+  users: UsersState,
+};
--- src/types.js
+++ src/types.js
@@ -6,16 +6,16 @@ import type { InputSelector } from 'reselect';
 
 import type {
   Auth,
-  Topic,
   Message,
+  Narrow,
   RealmEmojiState,
   RealmFilter,
-  Narrow,
-  Stream,
   Subscription,
+  Topic,
   User,
 } from './api/apiTypes';
 import type { AppStyles } from './styles/theme';
+import type { GlobalState, PresenceState } from './stateTypes';
 
 export type { ChildrenArray } from 'react';
 export type React$Node = Node; // eslint-disable-line flowtype/type-id-match
@@ -25,6 +25,7 @@ export type MapStateToProps = any; // { MapStateToProps } from 'react-redux';
 
 export type * from './actionTypes';
 export type * from './api/apiTypes';
+export type * from './stateTypes';
 
 export type ThunkDispatch<T> = ((Dispatch, GetState) => T) => T;
 
@@ -86,28 +87,6 @@ export type PmRecipientUser = {
   id: number,
   is_mirror_dummy: boolean,
   short_name: string,
-};
-
-/**
- * An index on `MessagesState`, listing messages in each narrow.
- *
- * Keys are `JSON.stringify`-encoded `Narrow` objects.
- * Values are sorted lists of message IDs.
- *
- * See also `MessagesState`, which stores the message data indexed by ID.
- */
-export type NarrowsState = {
-  [narrow: string]: number[],
-};
-
-/**
- * A map with all messages we've stored locally, indexed by ID.
- *
- * See also `NarrowsState`, which is an index on this data that identifies
- * messages belonging to a given narrow.
- */
-export type MessagesState = {
-  [id: number]: Message,
 };
 
 export type UserIdMap = {
@@ -196,10 +175,6 @@ export type Fetching = {
   newer: boolean,
 };
 
-export type StreamsState = Stream[];
-
-export type SubscriptionsState = Subscription[];
-
 export type HeartbeatEvent = {
   type: 'heartbeat',
   id: number,
@@ -233,73 +208,12 @@ export type EditMessage = {
   topic: string,
 };
 
-export type AccountsState = Account[];
-
 export type Debug = {
   highlightUnreadMessages: boolean,
   doNotMarkMessagesAsRead: boolean,
 };
 
-export type SessionState = {
-  eventQueueId: number,
-  editMessage: ?EditMessage,
-  isOnline: boolean,
-  isActive: boolean,
-  isHydrated: boolean,
-  needsInitialFetch: boolean,
-  orientation: Orientation,
-  outboxSending: boolean,
-  safeAreaInsets: Dimensions,
-  debug: Debug,
-};
-
-/**
- * Info about how completely we know the messages in each narrow of
- * MessagesState.
- */
-export type CaughtUpState = {
-  [narrow: string]: CaughtUp,
-};
-
-export type FetchingState = {
-  [narrow: string]: Fetching,
-};
-
-export type FlagsState = {
-  [flagName: string]: {
-    [messageId: number]: boolean,
-  },
-};
-
-export type MigrationsState = {
-  version?: string,
-};
-
-export type LoadingState = {
-  presence: boolean,
-  subscriptions: boolean,
-  streams: boolean,
-  unread: boolean,
-  users: boolean,
-};
-
 export type MuteTuple = [string, string];
-export type MuteState = MuteTuple[];
-
-export type NavigationState = {
-  index: number,
-  isTransitioning: boolean,
-  key: string,
-  routes: Array<{
-    key: string,
-    title: string,
-    routeName: string,
-    /** The fields in `params` vary by route; see `navActions.js`. */
-    params: {
-      narrow?: Narrow,
-    },
-  }>,
-};
 
 export type RealmBot = {
   email: string,
@@ -309,43 +223,9 @@ export type RealmBot = {
   user_id: number,
 };
 
-/**
- * State with general info about a Zulip organization; our state subtree `realm`.
- *
- * @prop twentyFourHourTime
- * @prop canCreateStreams
- * @prop crossRealmBots - The server's cross-realm bots; e.g., Welcome Bot.
- *   Cross-realm bots should be treated like normal bots.
- * @prop nonActiveUsers - All users in the organization with `is_active`
- *   false; for normal users, this means they or an admin deactivated their
- *   account.  See `User` and the linked documentation.
- * @prop pushToken
- * @prop filters
- * @prop emoji
- * @prop isAdmin
- */
-export type RealmState = {
-  twentyFourHourTime: boolean,
-  canCreateStreams: boolean,
-  crossRealmBots: RealmBot[],
-  nonActiveUsers: User[],
-  pushToken: {
-    token: string,
-    msg: string,
-    result: string,
-  },
-  filters: RealmFilter[],
-  emoji: RealmEmojiState,
-  isAdmin: boolean,
-};
-
 export type TopicExtended = Topic & {
   isMuted: boolean,
   unreadCount: number,
-};
-
-export type TopicsState = {
-  [number]: Topic[],
 };
 
 export type ThemeType = 'default' | 'night';
@@ -357,22 +237,6 @@ export type Context = {
 };
 
 export type StatusBarStyle = 'light-content' | 'dark-content';
-
-export type SettingsState = {
-  locale: string,
-  theme: ThemeType,
-  offlineNotification: boolean,
-  onlineNotification: boolean,
-  experimentalFeaturesEnabled: boolean,
-  streamNotification: boolean,
-};
-
-export type TypingState = {
-  [normalizedRecipients: string]: {
-    time: number,
-    userIds: number[],
-  },
-};
 
 /**
  * A message we're in the process of sending.
@@ -424,73 +288,6 @@ export type UnreadStreamsState = StreamUnreadItem[];
 export type UnreadHuddlesState = HuddlesUnreadItem[];
 export type UnreadPmsState = PmsUnreadItem[];
 export type UnreadMentionsState = number[];
-
-export type AlertWordsState = string[];
-
-export type DraftsState = {
-  [narrow: string]: string,
-};
-
-/**
- * A collection of (almost) all users in the Zulip org; our `users` state subtree.
- *
- * This contains all users except deactivated users and cross-realm bots.
- * For those, see RealmState.
- */
-export type UsersState = User[];
-
-export type UserGroupsState = UserGroup[];
-
-/**
- * The `presence` subtree of our Redux state.
- *
- * @prop (email) - Indexes over all users for which the app has received a
- *   presence status.
- */
-export type PresenceState = {
-  [email: string]: Presence,
-};
-
-export type OutboxState = Outbox[];
-
-export type UnreadState = {
-  streams: UnreadStreamsState,
-  huddles: UnreadHuddlesState,
-  pms: UnreadPmsState,
-  mentions: UnreadMentionsState,
-};
-
-/**
- * Our complete Redux state tree.
- *
- * Each property is a subtree maintained by its own reducer function.
- */
-export type GlobalState = {
-  accounts: AccountsState,
-  alertWords: AlertWordsState,
-  caughtUp: CaughtUpState,
-  drafts: DraftsState,
-  fetching: FetchingState,
-  flags: FlagsState,
-  migrations: MigrationsState,
-  loading: LoadingState,
-  messages: MessagesState,
-  mute: MuteState,
-  narrows: NarrowsState,
-  nav: NavigationState,
-  outbox: OutboxState,
-  presence: PresenceState,
-  realm: RealmState,
-  session: SessionState,
-  settings: SettingsState,
-  streams: StreamsState,
-  subscriptions: SubscriptionsState,
-  topics: TopicsState,
-  typing: TypingState,
-  unread: UnreadState,
-  userGroups: UserGroupsState,
-  users: UsersState,
-};
 
 /** A selector returning TResult. */
 // Seems like this should be OutputSelector... but for whatever reason,
