280e759e31265f71ec7c8f4496fd6e4d8ded9d5e
Greg Price <greg@zulipchat.com>
2019-01-10 20:02:14 -0800

    wip strict combineReducers type

--- flow-typed/redux_v4.x.x.js
+++ flow-typed/redux_v4.x.x.js
@@ -53,7 +53,7 @@ declare module 'redux' {
   declare export function bindActionCreators<A, C: ActionCreator<A, any>, D: DispatchAPI<A>>(actionCreator: C, dispatch: D): C;
   declare export function bindActionCreators<A, K, C: ActionCreators<K, A>, D: DispatchAPI<A>>(actionCreators: C, dispatch: D): C;
 
-  declare export function combineReducers<O: Object, A>(reducers: O): CombinedReducer<$ObjMap<O, <S>(r: Reducer<S, any>) => S>, A>;
+  declare export function combineReducers<O: Object, A>(reducers: O): CombinedReducer<$ObjMap<O, <S>(r: Reducer<S, A>) => S>, A>;
 
   declare export var compose: $Compose;
 }
