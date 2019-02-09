f82ebc7c3db6227301ce3713c6cfc2da220f156c
Greg Price <greg@zulipchat.com>
2019-02-08 21:00:12 -0800

    wip upgrade further
    The APIs that the migration doc took us to are themselves deprecated, lol.
    Well, some are -- for registration, which they've simplified further.
    
    Thanks to Android Studio for pointing out the deprecation.
    
    Followed the current upstream docs for FCM itself:
      https://firebase.google.com/docs/cloud-messaging/android/client#sample-register

--- android/app/src/main/java/com/zulipmobile/notifications/FcmListenerService.java
+++ android/app/src/main/java/com/zulipmobile/notifications/FcmListenerService.java
@@ -1,12 +1,24 @@
 package com.zulipmobile.notifications;
 
 import android.content.Context;
+import com.facebook.react.ReactApplication;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 import com.zulipmobile.MainApplication;
 import com.zulipmobile.notifications.NotificationHelper.ConversationMap;
 
 public class FcmListenerService extends FirebaseMessagingService {
+    /**
+     * Called by the Firebase framework when the InstanceID token is updated.
+     *
+     * Mainly this means on first registration, but the framework
+     * might choose to rotate the token later.
+     */
+    @Override
+    public void onNewToken(String token) {
+        NotificationsModule.emitToken((ReactApplication) getApplication(), token);
+    }
+
     @Override
     public void onMessageReceived(RemoteMessage message) {
         final Context applicationContext = getApplicationContext();
--- android/app/src/main/java/com/zulipmobile/notifications/NotificationsModule.java
+++ android/app/src/main/java/com/zulipmobile/notifications/NotificationsModule.java
@@ -1,12 +1,14 @@
 package com.zulipmobile.notifications;
 
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.util.Log;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.bridge.*;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
 
 import static com.zulipmobile.notifications.NotificationHelper.TAG;
 
@@ -26,15 +28,25 @@ public class NotificationsModule extends ReactContextBaseJavaModule {
     @Override
     public void initialize() {
         // Invoking `emitToken` here is a bit belt-and-suspenders: the FCM framework
-        // already invokes it (via `onRefreshToken`) at app startup.  But that can be
+        // already invokes it (via `onNewToken`) at app startup.  But that can be
         // before React is ready.  With some more care we could hang on to it and emit
         // the event a bit later, but instead we just redundantly emit here when we
         // know things have started up.
         getAndEmitToken((ReactApplication) getCurrentActivity().getApplication());
     }
 
-    static void getAndEmitToken(ReactApplication application) {
-        emitToken(application, FirebaseInstanceId.getInstance().getToken());
+    private static void getAndEmitToken(final ReactApplication application) {
+        FirebaseInstanceId.getInstance().getInstanceId()
+            .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+                @Override
+                public void onComplete(@NonNull Task<InstanceIdResult> task) {
+                    if (!task.isSuccessful()) {
+                        Log.w(TAG, "getInstanceId failed", task.getException());
+                        return;
+                    }
+                    emitToken(application, task.getResult().getToken());
+                }
+            });
     }
 
     static void emitToken(ReactApplication application, String token) {
@@ -44,7 +56,7 @@ public class NotificationsModule extends ReactContextBaseJavaModule {
                         .getReactInstanceManager()
                         .getCurrentReactContext();
         if (reactContext == null) {
-            // Perhaps this is possible if InstanceIDListenerService gets invoked?
+            // Perhaps this is possible if onNewToken gets invoked?
             // If so, the next time the app is launched, this method will be invoked again
             // by our NotificationsModule#initialize, by which point there certainly is
             // a React context; so we'll learn the new token then.
