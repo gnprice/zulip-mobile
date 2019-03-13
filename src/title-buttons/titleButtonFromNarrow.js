6a6da79855a3d3657f2f36be7a0960c54b12a41e
Greg Price <greg@zulipchat.com>
2019-03-12 18:13:46 -0700

    wip fold ExtraNavButton...

--- src/title-buttons/titleButtonFromNarrow.js
+++ src/title-buttons/titleButtonFromNarrow.js
@@ -1,8 +1,9 @@
 /* @flow strict-local */
-import React from 'react';
+import React, { PureComponent } from 'react';
 import type { ComponentType } from 'react';
+import { connect } from 'react-redux';
 
-import type { Narrow } from '../types';
+import type { Dispatch, Narrow, Stream } from '../types';
 import {
   isHomeNarrow,
   isPrivateNarrow,
@@ -10,12 +11,15 @@ import {
   isSpecialNarrow,
   isStreamNarrow,
   isTopicNarrow,
+  streamNarrow,
 } from '../utils/narrow';
+import { getStreams } from '../selectors';
+import NavButton from '../nav/NavButton';
+import { doNarrow, navigateToTopicList } from '../actions';
+
 import InfoNavButtonStream from './InfoNavButtonStream';
 import InfoNavButtonPrivate from './InfoNavButtonPrivate';
 import InfoNavButtonGroup from './InfoNavButtonGroup';
-import ExtraNavButtonStream from './ExtraNavButtonStream';
-import ExtraNavButtonTopic from './ExtraNavButtonTopic';
 
 type Props = {| color: string, narrow: Narrow |};
 type NarrowNavButton = ComponentType<Props>;
@@ -32,6 +36,56 @@ const infoButtonHandlers: NarrowNavButtonCandidate[] = [
   { isFunc: isPrivateNarrow, ButtonComponent: InfoNavButtonPrivate },
   { isFunc: isGroupNarrow, ButtonComponent: InfoNavButtonGroup },
 ];
+
+class _ExtraNavButtonStream extends PureComponent<{|
+  dispatch: Dispatch,
+  narrow: Narrow,
+  color: string,
+  streams: Stream[],
+|}> {
+  handlePress = () => {
+    const { dispatch, narrow, streams } = this.props;
+    const stream = streams.find(x => x.name === narrow[0].operand);
+    if (stream) {
+      dispatch(navigateToTopicList(stream.stream_id));
+    }
+  };
+
+  render() {
+    const { color } = this.props;
+
+    return <NavButton name="list" color={color} onPress={this.handlePress} />;
+  }
+}
+
+const ExtraNavButtonStream = connect((state, props) => ({
+  streams: getStreams(state),
+}))(_ExtraNavButtonStream);
+
+class _ExtraNavButtonTopic extends PureComponent<{|
+  dispatch: Dispatch,
+  narrow: Narrow,
+  color: string,
+  streams: Stream[],
+|}> {
+  handlePress = () => {
+    const { dispatch, narrow, streams } = this.props;
+    const stream = streams.find(x => x.name === narrow[0].operand);
+    if (stream) {
+      dispatch(doNarrow(streamNarrow(stream.name)));
+    }
+  };
+
+  render() {
+    const { color } = this.props;
+
+    return <NavButton name="arrow-up" color={color} onPress={this.handlePress} />;
+  }
+}
+
+const ExtraNavButtonTopic = connect((state, props) => ({
+  streams: getStreams(state),
+}))(_ExtraNavButtonTopic);
 
 const extraButtonHandlers: NarrowNavButtonCandidate[] = [
   { isFunc: isHomeNarrow, ButtonComponent: null },
