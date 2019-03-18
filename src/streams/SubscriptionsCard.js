a0e22696adc8157834b0b6b3eb9a4840c0fbd907
Greg Price <greg@zulipchat.com>
2019-03-18 15:18:06 -0700

    wip subs card: cut dead props.narrow
    I'm not sure if this has ever been used; it's a bit complicated to
    search for in history.  Certainly I'm having a hard time imagining
    what meaning it could have had.
    
    ... ah, that comment in `connect` is a hint -- it's from a version of
    this living in the nav drawer.

--- src/streams/SubscriptionsCard.js
+++ src/streams/SubscriptionsCard.js
@@ -4,9 +4,9 @@ import { connect } from 'react-redux';
 import React, { PureComponent } from 'react';
 import { View, StyleSheet } from 'react-native';
 
-import type { Dispatch, Narrow, Subscription, GlobalState } from '../types';
+import type { Dispatch, Subscription, GlobalState } from '../types';
 import StreamList from './StreamList';
-import { isStreamNarrow, streamNarrow } from '../utils/narrow';
+import { streamNarrow } from '../utils/narrow';
 import { getUnreadByStream } from '../selectors';
 import { getSubscribedStreams } from '../subscriptions/subscriptionSelectors';
 import { doNarrow } from '../actions';
@@ -20,7 +20,6 @@ const styles = StyleSheet.create({
 
 type Props = {|
   dispatch: Dispatch,
-  narrow: Narrow,
   subscriptions: Subscription[],
   unreadByStream: number[],
 |};
@@ -31,14 +30,12 @@ class SubscriptionsCard extends PureComponent<Props> {
   };
 
   render() {
-    const { narrow, subscriptions, unreadByStream } = this.props;
-    const selected = isStreamNarrow(narrow) && narrow[0].operand;
-
+    const { subscriptions, unreadByStream } = this.props;
     return (
       <View style={styles.container}>
         <StreamList
           streams={subscriptions}
-          selected={selected}
+          selected={false}
           unreadByStream={unreadByStream}
           onPress={this.handleNarrow}
         />
@@ -48,7 +45,6 @@ class SubscriptionsCard extends PureComponent<Props> {
 }
 
 export default connect((state: GlobalState, props) => ({
-  narrow: props.narrow || [],
   // Main screen no longer contains drawer,
   // so at any position we cannot show selected stream in the list
   // needs to be removed when we finalize navigation without drawer
