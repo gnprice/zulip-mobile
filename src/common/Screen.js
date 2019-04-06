f6951efe45a7bd8ac90425fa11841aff32c04ae9
Greg Price <greg@zulipchat.com>
2019-04-05 18:14:21 -0700

    clean out a bunch of cruft

--- src/common/Screen.js
+++ src/common/Screen.js
@@ -1,6 +1,5 @@
 /* @flow strict-local */
 import { connect } from '../react-redux';
-import type { Connected, OwnProps } from '../react-redux';
 
 import React, { PureComponent } from 'react';
 import type { ComponentType, ElementConfig, Node as React$Node } from 'react';
@@ -139,62 +138,8 @@ class Screen extends PureComponent<Props> {
   }
 }
 
-// CAUTION: These only function when actually forced to be instantiated.
-// E.g. a hoped-for
-//   type With<S, P> = S;
-// utters not a peep at
-//   const x: With<null, IsEqual<string, number>> = null;
-// Still, probably handy if used cleverly.
-type IsSupertype<+S, +T: S> = S;
-type IsSupertype2<+S, +T: S> = T; // a helper
-type IsSubtype<+S, +T> = IsSupertype2<T, S>; // i.e. S, if true
-type IsEqual<+S, +T: S> = IsSupertype2<T, S>; // i.e. S (== T), if true
-
-/*
- * Has type T, but only if T <= T1 -- and T, T1 both instantiable.
- *
- * E.g. Chain<S, IsEqual<S, T>> is S, but additionally requires S == T.
- */
-type Chain<T1, T: T1> = T;
-type And1<T1, T: T1> = T;
-type And2<T2, T1: T2, T: T1> = T;
-
-type IsElementwiseSubtype<+S, +T> =
-  $ObjMapi<S,
-    <K, V>(K, V) => IsSubtype<V, $ElementType<T, K>>
-  >;
-
-// Oddly, Flow accepts this declaration with <-U, -L> but also with <+U, +L>.
-type BoundedDiff<-U, -L> = $Diff<
-  IsSupertype<U, $ReadOnly<{| ...U, ...L |}>>,
-  $ObjMap<L, () => mixed>,
->;
-
-//type OwnProps<-C, -SP> = $Diff<BoundedDiff<ElementConfig<C>, SP>, {| dispatch: Dispatch |}>;
-
-function connect1<
-  SP,
-  P,
-  C: ComponentType<P>,
-  //</P>SP: IsElementwiseSubtype<$Exact<SP1>, ElementConfig<C>>,
-  >(mapStateToProps: GlobalState => SP): C => ComponentType<OwnProps<C, SP>> {
-  return connect(mapStateToProps);
-}
-
 const msp = (state: GlobalState) => ({
   safeAreaInsets: ((32: $FlowFixMe): Dimensions),
 });
 
-const cr = connect(msp);
-
-//const c: ComponentType<OwnProps<typeof Screen, $Call<typeof msp, empty>>> = cr(Screen);
-const c = cr(Screen);
-
-export default c;
-
-export const foo: Connected<typeof msp, typeof Screen> = connect(msp)(Screen);
-
-// bad -- infers empty
-// export const cr2: <C: ComponentType<*>>(C) => Connected<typeof msp, C> = connect(msp);
-
-export const c3 = connect(msp)(Screen);
+export default connect(msp)(Screen);
--- src/react-redux.js
+++ src/react-redux.js
@@ -4,31 +4,7 @@ import { connect as connectInner } from 'react-redux';
 
 import type { GlobalState, Dispatch } from './types';
 
-
-// CAUTION: These only function when actually forced to be instantiated.
-// E.g. a hoped-for
-//   type With<S, P> = S;
-// utters not a peep at
-//   const x: With<null, IsEqual<string, number>> = null;
-// Still, probably handy if used cleverly.
 type IsSupertype<+S, +T: S> = S;
-type IsSupertype2<+S, +T: S> = T; // a helper
-type IsSubtype<+S, +T> = IsSupertype2<T, S>; // i.e. S, if true
-type IsEqual<+S, +T: S> = IsSupertype2<T, S>; // i.e. S (== T), if true
-
-/*
- * Has type T, but only if T <= T1 -- and T, T1 both instantiable.
- *
- * E.g. Chain<S, IsEqual<S, T>> is S, but additionally requires S == T.
- */
-type Chain<T1, T: T1> = T;
-type And1<T1, T: T1> = T;
-type And2<T2, T1: T2, T: T1> = T;
-
-type IsElementwiseSubtype<+S, +T> =
-  $ObjMapi<S,
-    <K, V>(K, V) => IsSubtype<V, $ElementType<T, K>>
-  >;
 
 // Oddly, Flow accepts this declaration with <-U, -L> but also with <+U, +L>.
 export type BoundedDiff<-U, -L> = $Diff<
@@ -37,26 +13,6 @@ export type BoundedDiff<-U, -L> = $Diff<
 >;
 
 export type OwnProps<-C, -SP> = $Diff<BoundedDiff<ElementConfig<C>, SP>, {| dispatch: Dispatch |}>;
-
-export type Connected<MSP, C> = ComponentType<OwnProps<C, $Call<MSP, empty>>>;
-
-function connect1<
-  SP,
-  P,
-  C: ComponentType<P>,
-  //</P>SP: IsElementwiseSubtype<$Exact<SP1>, ElementConfig<C>>,
-  >(mapStateToProps: GlobalState => SP): C => ComponentType<OwnProps<C, SP>> {
-  return connectInner(mapStateToProps);
-}
-
-//export const connect = connectInner;
-
-type Connect<SP, P, C: ComponentType<P>> =
-  (GlobalState => SP) =>
-     C => ComponentType<OwnProps<C, SP>>;
-
-// export const connect2: <SP, P, C: ComponentType<P>>(GlobalState => SP) =>
-//   C => ComponentType<OwnProps<C, SP>> = connectInner;
 
 export const connect: <SP, P, C: ComponentType<P>>(GlobalState => SP) =>
    C => ComponentType<OwnProps<C, SP>> = (msp) => connectInner(msp);
