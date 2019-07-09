b6c3f89c525ac0ebbf6676770b10cf982107726c
Greg Price <greg@zulipchat.com>
2019-07-08 17:39:16 -0700

    wip compare with fetch_initial_state_data
    As of zulip/zulip@dae142a6a .

--- src/api/initialDataTypes.js
+++ src/api/initialDataTypes.js
@@ -38,28 +38,56 @@ export type PmsUnreadItem = {|
   unread_message_ids: number[],
 |};
 
+/*
+ * ABOUT THESE TYPES
+ *
+ * These properties and their types correspond to `fetch_initial_state_data`
+ * in zulip/zulip:zerver/lib/events.py .  See background docs linked in the
+ * jsdoc for `InitialData`.
+ *
+ * Each specific object type below corresponds to a value that can be part
+ * of the `event_types` passed to that function, corresponding to
+ * `fetch_event_types` in the /register API:
+ *   https://zulipchat.com/api/register-queue
+ *
+ * The `fetch_initial_state_data` function is organized by these event
+ * types, so we organize this list the same way to make it easy to compare.
+ */
+
+// Data always included regardless of `event_types`.
 type InitialDataBase = {|
   last_event_id: number,
   msg: string,
   queue_id: number,
 |};
 
+// For `alert_words`.
 type InitialDataAlertWords = {|
   alert_words: string[],
 |};
 
+// TODO: custom_profile_fields
+
+// TODO: hotspots
+
+// For `message`.
 type InitialDataMessage = {|
   max_message_id: number,
 |};
 
+// Etc., etc.
 type InitialDataMutedTopics = {|
   muted_topics: MuteTuple[],
 |};
+
+// TODO: pointer
 
 type InitialDataPresence = {|
   presences: {| [email: string]: UserPresence |},
 |};
 
+// TODO check this against server code, add missing stuff
+// NB the `property_types` bit which provides most of these.
 type InitialDataRealm = {|
   max_icon_file_size: number,
   realm_add_emoji_by_admins_only: boolean,
@@ -104,6 +132,8 @@ type InitialDataRealm = {|
   realm_waiting_period_threshold: number,
 |};
 
+// TODO realm_domains
+
 type InitialDataRealmEmoji = {|
   realm_emoji: RealmEmojiById,
 |};
@@ -112,64 +142,44 @@ type InitialDataRealmFilters = {|
   realm_filters: RealmFilter[],
 |};
 
-type InitialDataRealmUser = {|
-  avatar_source: 'G',
-  avatar_url: string | null,
-  avatar_url_medium: string,
-  can_create_streams: boolean,
-  cross_realm_bots: CrossRealmBot[],
-  email: string,
-  enter_sends: boolean,
-  full_name: string,
-  is_admin: boolean,
-  realm_non_active_users: User[],
-  realm_users: User[],
-  user_id: number,
-|};
-
 type InitialDataRealmUserGroups = {|
   realm_user_groups: UserGroup[],
 |};
 
-type InitialDataStream = {|
-  streams: Stream[],
+/** Despite the name, mostly about *this* user. */
+type InitialDataRealmUser = {|
+  // raw_users
+  avatar_source: 'G',
+  avatar_url_medium: string,
+  avatar_url: string | null,
+  can_create_streams: boolean,
+  // can_subscribe_other_users
+  cross_realm_bots: CrossRealmBot[], // About other users.
+  is_admin: boolean,
+  // is_guest
+  user_id: number,
+  enter_sends: boolean,
+  email: string,
+  // delivery_email
+  full_name: string,
+
+  realm_non_active_users: User[], // TODO ??
+  realm_users: User[], // TODO ??
 |};
+
+// TODO realm_bots
+
+// TODO realm_embedded_bots
+
+// TODO recent_private_conversations
 
 type InitialDataSubscription = {|
-  never_subscribed: NeverSubscribedStream[],
   subscriptions: Subscription[],
   unsubscribed: Subscription[],
+  never_subscribed: NeverSubscribedStream[],
 |};
 
-type InitialDataUpdateDisplaySettings = {|
-  default_language: string,
-  emojiset: string,
-  emojiset_choices: { [string]: string },
-  high_contrast_mode: boolean,
-  left_side_userlist: boolean,
-  night_mode: boolean,
-  timezone: string,
-  translate_emoticons: boolean,
-  twenty_four_hour_time: boolean,
-|};
-
-type InitialDataUpdateGlobalNotifications = {|
-  default_desktop_notifications: boolean,
-  enable_desktop_notifications: boolean,
-  enable_digest_emails: boolean,
-  enable_offline_email_notifications: boolean,
-  enable_offline_push_notifications: boolean,
-  enable_online_push_notifications: boolean,
-  enable_sounds: boolean,
-  enable_stream_desktop_notifications: boolean,
-  enable_stream_email_notifications: boolean,
-  enable_stream_push_notifications: boolean,
-  enable_stream_sounds: boolean,
-  message_content_in_email_notifications: boolean,
-  pm_content_in_desktop_notifications: boolean,
-  realm_name_in_notifications: boolean,
-|};
-
+// NB this comes only with `update_message_flags` *plus* `message`.
 type InitialDataUpdateMessageFlags = {|
   unread_msgs: {
     streams: StreamUnreadItem[],
@@ -180,6 +190,59 @@ type InitialDataUpdateMessageFlags = {|
   },
 |};
 
+// TODO starred_messages
+
+type InitialDataStream = {|
+  streams: Stream[],
+  // stream_name_max_length
+  // stream_description_max_length
+|};
+
+// TODO default_stream
+
+// TODO default_stream_groups
+
+// TODO stop_words
+
+// NB most of these come from `property_types`.
+type InitialDataUpdateDisplaySettings = {|
+  default_language: string,
+  // demote_inactive_streams
+  // dense_mode
+  emojiset: string,
+  emojiset_choices: { [string]: string },
+  left_side_userlist: boolean,
+  timezone: string,
+  twenty_four_hour_time: boolean,
+  high_contrast_mode: boolean,
+  night_mode: boolean,
+  translate_emoticons: boolean,
+  // starred_message_counts
+  // fluid_layout_width
+|};
+
+// NB most of these come from `notification_setting_types`.
+type InitialDataUpdateGlobalNotifications = {|
+  default_desktop_notifications: boolean, // TODO ??
+  enable_desktop_notifications: boolean,
+  enable_digest_emails: boolean,
+  // enable_login_emails
+  enable_offline_email_notifications: boolean,
+  enable_offline_push_notifications: boolean,
+  enable_online_push_notifications: boolean,
+  enable_sounds: boolean,
+  enable_stream_desktop_notifications: boolean,
+  enable_stream_email_notifications: boolean,
+  enable_stream_push_notifications: boolean,
+  // enable_stream_audible_notifications
+  enable_stream_sounds: boolean, // TODO ??
+  message_content_in_email_notifications: boolean,
+  // notification_sound
+  pm_content_in_desktop_notifications: boolean,
+  realm_name_in_notifications: boolean,
+  // available_notification_sounds
+|};
+
 type InitialDataUserStatus = {|
   /**
    * Older servers (through at least 1.9.1) don't send this.
@@ -188,6 +251,18 @@ type InitialDataUserStatus = {|
   user_status?: UserStatusMapObject,
 |};
 
+// TODO zulip_version
+
+/**
+ * Initial data snapshot returned in a /register response.
+ *
+ * For background on this system, see our docs from a client perspective:
+ *   https://github.com/zulip/zulip-mobile/blob/master/docs/architecture/realtime.md
+ * and a mainly server-side perspective:
+ *   https://zulip.readthedocs.io/en/latest/subsystems/events-system.html
+ *
+ * For more on the many properties here, see comments above in this file.
+ */
 export type InitialData = {|
   ...InitialDataBase,
   ...InitialDataAlertWords,
