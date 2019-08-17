3170fbb8836bfecad2ba863b3ad253fd8acf6806
Greg Price <greg@zulipchat.com>
2019-08-16 19:15:22 -0700

    further simplify RealmScreen

--- src/start/RealmScreen.js
+++ src/start/RealmScreen.js
@@ -1,64 +1,41 @@
 /* @flow strict-local */
 import React, { PureComponent } from 'react';
 import { ScrollView, TextInput, Keyboard } from 'react-native';
-import type { NavigationScreenProp } from 'react-navigation';
 
 import type { Dispatch } from '../types';
-import { connectFlowFixMe } from '../react-redux';
-import { ErrorMsg, Label, Screen, ZulipButton } from '../common';
-import { isValidUrl } from '../utils/url';
+import { connect } from '../react-redux';
+import { Screen, ZulipButton } from '../common';
 import { realmAdd, loginSuccess } from '../actions';
 import styles from '../styles';
 
 type Props = {|
   dispatch: Dispatch,
-  navigation: NavigationScreenProp<mixed>,
-  initialRealm: string,
 |};
 
 type State = {|
   realm: string,
-  error: string | null,
-  progress: boolean,
 |};
 
 class RealmScreen extends PureComponent<Props, State> {
   state = {
-    progress: false,
-    realm: this.props.initialRealm,
-    error: null,
+    realm: '',
   };
 
   scrollView: ScrollView;
 
   tryRealm = async () => {
     const { realm } = this.state;
-
-    this.setState({
-      realm,
-      progress: true,
-      error: null,
-    });
-
     const { dispatch } = this.props;
-
-    try {
 	dispatch(realmAdd(realm));
 	const fetchedKey = {email: 'x@y.z', api_key: '012345abcd'};
 	dispatch(loginSuccess(realm, fetchedKey.email, fetchedKey.api_key));
       Keyboard.dismiss();
-    } catch (err) {
-      this.setState({ error: 'Cannot connect to server' });
-    } finally {
-      this.setState({ progress: false });
-    }
   };
 
   handleRealmChange = (value: string) => this.setState({ realm: value });
 
   render() {
-    const { initialRealm, navigation } = this.props;
-    const { progress, error, realm } = this.state;
+    const { realm } = this.state;
 
     return (
       <Screen title="Welcome" padding centerContent keyboardShouldPersistTaps="always">
@@ -69,11 +46,10 @@ class RealmScreen extends PureComponent<Props, State> {
           onSubmitEditing={this.tryRealm}
           enablesReturnKeyAutomatically
 />
-        {error !== null && <ErrorMsg error={error} />}
         <ZulipButton
           style={styles.halfMarginTop}
           text="Enter"
-          progress={progress}
+          progress={false}
           onPress={this.tryRealm}
       disabled={false}
         />
@@ -82,6 +58,4 @@ class RealmScreen extends PureComponent<Props, State> {
   }
 }
 
-export default connectFlowFixMe((state, props) => ({
-    initialRealm: '',
-}))(RealmScreen);
+export default connect()(RealmScreen);
