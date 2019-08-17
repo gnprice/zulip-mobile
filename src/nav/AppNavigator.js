9187d3962cdd5e76d919211a2c444ebb5c83d065
Greg Price <greg@zulipchat.com>
2019-08-16 18:48:34 -0700

    Cut phantom nav, and auth screen code.

--- src/nav/AppNavigator.js
+++ src/nav/AppNavigator.js
@@ -3,19 +3,15 @@ import { StackNavigator } from 'react-navigation';
 
 import AccountPickScreen from '../account/AccountPickScreen';
 import RealmScreen from '../start/RealmScreen';
-import AuthScreen from '../start/AuthScreen';
 import MainScreenWithTabs from '../main/MainScreenWithTabs';
 import LoadingScreen from '../start/LoadingScreen';
-import PasswordAuthScreen from '../start/PasswordAuthScreen';
 
 export default StackNavigator(
   // $FlowFixMe react-navigation types :-/ -- see a36814e80
   {
     account: { screen: AccountPickScreen },
-    auth: { screen: AuthScreen },
     loading: { screen: LoadingScreen },
     main: { screen: MainScreenWithTabs },
-    password: { screen: PasswordAuthScreen },
     realm: { screen: RealmScreen },
   },
   {
--- src/nav/navActions.js
+++ src/nav/navActions.js
@@ -15,96 +15,11 @@ export const navigateBack = () => (dispatch: Dispatch, getState: GetState): Navi
   // $FlowFixMe
   dispatch(NavigationActions.pop({ n: getSameRoutesCount(getState()) }));
 
-/** Only call this via `doNarrow`.  See there for details. */
-export const navigateToChat = (narrow: Narrow): NavigateAction =>
-  NavigationActions.navigate({ routeName: 'chat', params: { narrow } });
-
-export const navigateToAllStreams = (): NavigateAction =>
-  NavigationActions.navigate({ routeName: 'subscriptions' });
-
-export const navigateToUsersScreen = (): NavigateAction =>
-  NavigationActions.navigate({ routeName: 'users' });
-
-export const navigateToSearch = (): NavigateAction =>
-  NavigationActions.navigate({ routeName: 'search' });
-
-export const navigateToSettings = (): NavigateAction =>
-  NavigationActions.navigate({ routeName: 'settings' });
-
-export const navigateToEmojiPicker = (messageId: number): NavigateAction =>
-  NavigationActions.navigate({ routeName: 'emoji-picker', params: { messageId } });
-
-export const navigateToAuth = (): NavigateAction =>
-  NavigationActions.navigate({ routeName: 'auth' });
-
-export const navigateToPassword = (requireEmailFormat: boolean): NavigateAction =>
-  NavigationActions.navigate({ routeName: 'password', params: { requireEmailFormat } });
-
 export const navigateToAccountPicker = (): NavigateAction =>
   NavigationActions.navigate({ routeName: 'account' });
-
-export const navigateToAccountDetails = (email: string): NavigateAction =>
-  NavigationActions.navigate({ routeName: 'account-details', params: { email } });
-
-export const navigateToGroupDetails = (recipients: UserOrBot[]): NavigateAction =>
-  NavigationActions.navigate({ routeName: 'group-details', params: { recipients } });
 
 export const navigateToRealmScreen = (realm?: string): NavigateAction =>
   NavigationActions.navigate({ routeName: 'realm', params: { realm } });
 
-export const navigateToLightbox = (src: string, message: Message): NavigateAction =>
-  NavigationActions.navigate({ routeName: 'lightbox', params: { src, message } });
-
 export const navigateToLoading = (): NavigateAction =>
   NavigationActions.navigate({ routeName: 'loading' });
-
-export const navigateToLanguage = (): NavigateAction =>
-  NavigationActions.navigate({ routeName: 'language' });
-
-export const navigateToCreateGroup = (): NavigateAction =>
-  NavigationActions.navigate({ routeName: 'group' });
-
-export const navigateToDiagnostics = (): NavigateAction =>
-  NavigationActions.navigate({ routeName: 'diagnostics' });
-
-export const navigateToWelcomeHelp = (): NavigateAction =>
-  NavigationActions.navigate({ routeName: 'welcome-help' });
-
-export const navigateToWelcomeScreen = (): NavigateAction =>
-  NavigationActions.navigate({ routeName: 'welcome' });
-
-export const navigateToVariables = (): NavigateAction =>
-  NavigationActions.navigate({ routeName: 'variables' });
-
-export const navigateToTiming = (): NavigateAction =>
-  NavigationActions.navigate({ routeName: 'timing' });
-
-export const navigateToStorage = (): NavigateAction =>
-  NavigationActions.navigate({ routeName: 'storage' });
-
-export const navigateToDebug = (): NavigateAction =>
-  NavigationActions.navigate({ routeName: 'debug' });
-
-export const navigateToStream = (streamId: number): NavigateAction =>
-  NavigationActions.navigate({ routeName: 'stream', params: { streamId } });
-
-export const navigateToTopicList = (streamId: number): NavigateAction =>
-  NavigationActions.navigate({ routeName: 'topics', params: { streamId } });
-
-export const navigateToCreateStream = (): NavigateAction =>
-  NavigationActions.navigate({ routeName: 'stream-create' });
-
-export const navigateToEditStream = (streamId: number): NavigateAction =>
-  NavigationActions.navigate({ routeName: 'stream-edit', params: { streamId } });
-
-export const navigateToStreamSubscribers = (streamId: number): NavigateAction =>
-  NavigationActions.navigate({ routeName: 'invite-users', params: { streamId } });
-
-export const navigateToNotifications = (): NavigateAction =>
-  NavigationActions.navigate({ routeName: 'notifications' });
-
-export const navigateToLegal = (): NavigateAction =>
-  NavigationActions.navigate({ routeName: 'legal' });
-
-export const navigateToUserStatus = (): NavigateAction =>
-  NavigationActions.navigate({ routeName: 'user-status' });
