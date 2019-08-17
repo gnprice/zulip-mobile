a02fabb83301ff013842c81c7031b857ec5d5033
Greg Price <greg@zulipchat.com>
2019-08-16 17:31:19 -0700

    git rh ; rg -lF 'render()' src/ | grep -ve ^src/start/ -e /Screen -e ^src/main/ -e Profile -e ^src/common/ -e Account -e Provider -e src/nav/ -e animation -e AppNavigator | xargs git rm ; git rm -r src/nav/ChatNavBar.js src/message/messageActionSheet.js src/main/{StreamTabs,HomeTab}.js src/account-info/AccountDetails.js  src/title-buttons/titleButtonFromNarrow.js src/webview/ src/main/MainTabs.js src/compose/ src/account-info/AccountDetailsScreen.js ; yarn flow

--- src/webview/css/cssEmojis.js
+++
@@ -1,9 +0,0 @@
-/* @flow strict-local */
-import { codeToEmojiMap } from '../../emoji/data';
-
-const codeToCss = (code: string): string =>
-  `.emoji-${code}:before { content: '${codeToEmojiMap[code]}'; }`;
-
-export default Object.keys(codeToEmojiMap)
-  .map(key => codeToCss(key))
-  .join('\n');
