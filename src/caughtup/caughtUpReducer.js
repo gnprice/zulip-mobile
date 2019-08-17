3b5ab32db44f5f69fd4d892f1893d8014cb50c1d
Greg Price <greg@zulipchat.com>
2019-08-16 18:05:29 -0700

    and cut actual reducers

--- src/caughtup/caughtUpReducer.js
+++
@@ -1,9 +0,0 @@
-/* @flow strict-local */
-import type { CaughtUpState, Action } from '../types';
-import { NULL_OBJECT } from '../nullObjects';
-
-const initialState: CaughtUpState = NULL_OBJECT;
-
-export default (state: CaughtUpState = initialState, action: Action): CaughtUpState => {
-    return state;
-};
--- src/chat/fetchingReducer.js
+++
@@ -1,9 +0,0 @@
-/* @flow strict-local */
-import type { FetchingState, Action } from '../types';
-import { NULL_OBJECT } from '../nullObjects';
-
-const initialState: FetchingState = NULL_OBJECT;
-
-export default (state: FetchingState = initialState, action: Action): FetchingState => {
-    return state;
-};
