c8f937e7eac401fa706cf0ac314921adba88e53a
Greg Price <greg@zulipchat.com>
2019-08-16 19:22:29 -0700

    fold in AccountList

--- src/account/AccountPickScreen.js
+++ src/account/AccountPickScreen.js
@@ -1,14 +1,15 @@
 /* @flow strict-local */
 
 import React, { PureComponent } from 'react';
+import { View, FlatList } from 'react-native';
 
 import type { Dispatch } from '../types';
 import { connect } from '../react-redux';
 import { getAccountStatuses } from '../selectors';
 import type { AccountStatus } from './accountsSelectors';
 import { Centerer, ZulipButton, Logo, Screen } from '../common';
-import AccountList from './AccountList';
 import { navigateToRealmScreen, removeAccount } from '../actions';
+import AccountItem from './AccountItem';
 
 type Props = {|
   accounts: AccountStatus[],
@@ -29,12 +30,21 @@ class AccountPickScreen extends PureComponent<Props> {
     return (
       <Screen title="Pick account" centerContent padding canGoBack={false}>
         <Centerer>
-          {accounts.length === 0 && <Logo />}
-          <AccountList
-            accounts={accounts}
-            onAccountSelect={this.handleAccountSelect}
-            onAccountRemove={this.handleAccountRemove}
-          />
+            {accounts.length === 0 && <Logo />}
+      <View>
+        <FlatList
+          data={accounts}
+          keyExtractor={item => item.realm}
+          renderItem={({ item, index }) => (
+            <AccountItem
+              index={index}
+              realm={item.realm}
+              onSelect={this.handleAccountSelect}
+              onRemove={this.handleAccountRemove}
+            />
+          )}
+        />
+      </View>
             <ZulipButton
 	style={{marginTop: 16}}
             text="Add new account"
