c85bcdf8184ffd9fdc230657a8b6dc159bb9ad85
Greg Price <greg@zulipchat.com>
2019-08-19 13:03:10 -0700

    cut migrations

--- src/boot/store.js
+++ src/boot/store.js
@@ -8,7 +8,6 @@ import type { Action, GlobalState } from '../types';
 import rootReducer from './reducers';
 import middleware from './middleware';
 import ZulipAsyncStorage from './ZulipAsyncStorage';
-import createMigration from '../redux-persist-migrate/index';
 
 // AsyncStorage.clear(); // use to reset storage during development
 
@@ -46,63 +45,6 @@ export const storeKeys: Array<$Keys<GlobalState>> = [
 export const cacheKeys: Array<$Keys<GlobalState>> = [
 ];
 
-/**
- * Drop all server data, as a rehydrate-time migration.
- *
- * Most of our data is just copied from the server, and gets routinely
- * discarded any time the event queue expires and we make a new `/register`
- * call.  That's much more frequent than a new app release, let alone one
- * with a data migration... so forcing the same thing in a migration is
- * inexpensive, and makes a simple way to handle most migrations.
- *
- * One important difference from an expired event queue: `DEAD_QUEUE`
- * leaves the stale data mostly in place, to be clobbered by fresh data
- * by the subsequent `REALM_INIT`.  Here, because the old data may not work
- * with the current code at all, we have to actually discard it up front.
- * The behavior is similar to `ACCOUNT_SWITCH`, which also discards most
- * data: we'll show the loading screen while fetching initial data.  See
- * the `REHYDRATE` handlers in `sessionReducer` and `navReducer` for how
- * that happens.
- */
-function dropCache(state: GlobalState): $Shape<GlobalState> {
-  const result: $Shape<GlobalState> = {};
-  storeKeys.forEach(key => {
-    // $FlowFixMe This is well-typed only because it's the same `key` twice.
-    result[key] = state[key];
-  });
-  return result;
-}
-
-const migrations: { [string]: (GlobalState) => GlobalState } = {
-  // The type is a lie, in several ways:
-  //  * The actual object contains only the properties we persist:
-  //    those in `storeKeys` and `cacheKeys`, but not `discardKeys`.
-  //  * The actual input is from an older version of the code, one with
-  //    different data structures -- after all, that's the point of the
-  //    migration -- which usually have a different type.
-  //  * For all but the latest migration, the same is true of the output.
-  //
-  // Still, it seems a more helpful approximation than nothing.  Where the
-  // falsehoods show through, we freely tell Flow to ignore them.
-
-  // Example if removing a top-level subtree entirely:
-  //   import { AsyncStorage } from 'react-native';
-  //   ...
-  //   AsyncStorage.removeItem('reduxPersist:messages');
-
-  '6': state => ({
-    // This rolls up all previous migrations, to clean up after our bug #3553.
-    // Mostly we can just `dropCache`, to reload data from the server...
-    ...dropCache(state),
-    accounts: state.accounts.map(a => ({
-      ...a,
-    })),
-  }),
-
-  // $FlowMigrationFudge
-  '7': dropCache,
-};
-
 const reduxPersistConfig: Config = {
   whitelist: [...storeKeys, ...cacheKeys],
   // $FlowFixMe: https://github.com/rt2zz/redux-persist/issues/823
@@ -113,7 +55,6 @@ const store: Store<*, Action> = createStore(
   rootReducer,
   undefined,
   compose(
-    createMigration(migrations, 'migrations'),
     applyMiddleware(...middleware),
     autoRehydrate(),
   ),
