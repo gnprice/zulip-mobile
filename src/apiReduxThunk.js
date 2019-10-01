c99f4a859c5454e114f75da47f59f70d76bd7848
Greg Price <greg@zulipchat.com>
2019-09-30 19:32:44 -0700

    start attempting to type wrapped-api

--- src/apiReduxThunk.js
+++ src/apiReduxThunk.js
@@ -2,6 +2,25 @@
 import type { Auth, Dispatch, GetState, GlobalState } from './types';
 import * as api from './api';
 import { getAuth } from './account/accountsSelectors';
+import { objectFromEntries } from './jsBackport';
+
+// prettier-ignore
+type ApplyAuth =
+ & <R>((Auth, ...Array<empty>) => R) => () => R
+ & <T1, R>((Auth, T1, ...Array<empty>) => R) => (T1) => R
+ ;
+
+type AuthedApi = $ObjMap<typeof api, ApplyAuth>;
+
+const x = ((null: $FlowFixMe): AuthedApi);
+
+const u = x.getUsers();
+const r2 = x.deleteStream(); // TODO no error -- should require number arg
+
+/*
+const wrapApi = (auth: Auth): $ObjMap<typeof api, ApplyAuth> =>
+  objectFromEntries(Object.keys(api).map(name => [name, (...args) => api[name](auth, ...args)]));
+*/
 
 export function withApi(
   f: (api: typeof api, auth: Auth, dispatch: Dispatch, state: GlobalState) => Promise<mixed>,
