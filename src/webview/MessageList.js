1da0f182a4afad23fc3ff93e6cfc209bcff78e0e
Greg Price <greg@zulipchat.com>
2020-02-07 18:44:13 -0800

    wip pull to top level

--- src/webview/MessageList.js
+++ src/webview/MessageList.js
@@ -151,6 +151,61 @@ const assetsPath = Platform.OS === 'ios' ? './webview' : 'file:///android_asset/
 // [4] https://github.com/react-native-community/react-native-webview/blob/v5.12.1/ios/RNCWKWebView.m#L376
 //     https://github.com/facebook/react-native/blob/v0.59.10/React/Base/RCTConvert.m#L85
 
+/**
+ * Effective URL of the MessageList webview.
+ *
+ * We provide all HTML at creation (or refresh) time; the document named
+ * here is not loaded, and doesn't even need to exist. It serves only as a
+ * placeholder, so that relative URLs and cross-domain security restrictions
+ * have somewhere to believe that this document originates from.
+ */
+const baseUrl = `${assetsPath}/index.html`;
+
+// Paranoia^WSecurity: only load `baseUrl`, and only load it once. Any other
+// requests should be handed off to the OS, not loaded inside the WebView.
+const onShouldStartLoadWithRequest: (event: WebViewNavigation) => boolean = (() => {
+  // Inner closure to actually test the URL.
+  const urlTester: (url: string) => boolean = (() => {
+    // On Android this function is documented to be skipped on first load:
+    // therefore, simply never return true.
+    if (Platform.OS === 'android') {
+      return (url: string) => false;
+    }
+
+    // Otherwise (for iOS), return a closure that evaluates to `true` _exactly
+    // once_, and even then only if the URL looks like what we're expecting.
+    let loaded_once = false;
+    // The baseUrl, with its relative portion (if any) stripped.
+    const baseUrlTail = baseUrl.replace(/^\.\//, '');
+    // Disallow such monstrosities as `evilsite.com/?./webview/index.html`.
+    const unsafeUrlRegex = /[&?]/;
+    return (url: string) => {
+      if (!loaded_once) {
+        // The exact URL that will be loaded could be determined statically on
+        // Android. On iOS, though, it involves some unpredictable UUIDs which
+        // RN provides no good interface to. (`react-native-fs` is awful.)
+        if (url.startsWith('file://') && url.endsWith(baseUrlTail) && !unsafeUrlRegex.test(url)) {
+          loaded_once = true;
+          return true;
+        }
+      }
+      return false;
+    };
+  })();
+
+  // Outer closure to perform logging.
+  return (event: WebViewNavigation) => {
+    const ok = urlTester(event.url);
+    if (!ok) {
+      logging.warn('webview: rejected navigation event', {
+        navigation_event: { ...event },
+        expected_url: baseUrl,
+      });
+    }
+    return ok;
+  };
+})();
+
 class MessageList extends Component<Props> {
   static contextType = ThemeContext;
   context: ThemeColors;
@@ -243,65 +298,6 @@ class MessageList extends Component<Props> {
       auth,
       showMessagePlaceholders,
     });
-
-    /**
-     * Effective URL of the MessageList webview.
-     *
-     * We provide all HTML at creation (or refresh) time; the document named
-     * here is not loaded, and doesn't even need to exist. It serves only as a
-     * placeholder, so that relative URLs and cross-domain security restrictions
-     * have somewhere to believe that this document originates from.
-     */
-    const baseUrl = `${assetsPath}/index.html`;
-
-    // Paranoia^WSecurity: only load `baseUrl`, and only load it once. Any other
-    // requests should be handed off to the OS, not loaded inside the WebView.
-    const onShouldStartLoadWithRequest: (event: WebViewNavigation) => boolean = (() => {
-      // Inner closure to actually test the URL.
-      const urlTester: (url: string) => boolean = (() => {
-        // On Android this function is documented to be skipped on first load:
-        // therefore, simply never return true.
-        if (Platform.OS === 'android') {
-          return (url: string) => false;
-        }
-
-        // Otherwise (for iOS), return a closure that evaluates to `true` _exactly
-        // once_, and even then only if the URL looks like what we're expecting.
-        let loaded_once = false;
-        // The baseUrl, with its relative portion (if any) stripped.
-        const baseUrlTail = baseUrl.replace(/^\.\//, '');
-        // Disallow such monstrosities as `evilsite.com/?./webview/index.html`.
-        const unsafeUrlRegex = /[&?]/;
-        return (url: string) => {
-          if (!loaded_once) {
-            // The exact URL that will be loaded could be determined statically on
-            // Android. On iOS, though, it involves some unpredictable UUIDs which
-            // RN provides no good interface to. (`react-native-fs` is awful.)
-            if (
-              url.startsWith('file://')
-              && url.endsWith(baseUrlTail)
-              && !unsafeUrlRegex.test(url)
-            ) {
-              loaded_once = true;
-              return true;
-            }
-          }
-          return false;
-        };
-      })();
-
-      // Outer closure to perform logging.
-      return (event: WebViewNavigation) => {
-        const ok = urlTester(event.url);
-        if (!ok) {
-          logging.warn('webview: rejected navigation event', {
-            navigation_event: { ...event },
-            expected_url: baseUrl,
-          });
-        }
-        return ok;
-      };
-    })();
 
     // The `originWhitelist` and `onShouldStartLoadWithRequest` props are
     // meant to mitigate possible XSS bugs, by interrupting an attempted
