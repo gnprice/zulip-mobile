6bb94feba671b938f2184910796d13e204a2bfb5
Greg Price <greg@zulipchat.com>
2020-02-18 18:35:11 -0800

    wip and inline -- TODO move tests

--- src/chat/ChatScreen.js
+++ src/chat/ChatScreen.js
@@ -9,7 +9,8 @@ import { connect } from '../react-redux';
 import { OfflineNotice, ZulipStatusBar } from '../common';
 import Chat from './Chat';
 import ChatNavBar from '../nav/ChatNavBar';
-import { getTitleBackgroundColor } from '../title/titleSelectors';
+import { DEFAULT_TITLE_BACKGROUND_COLOR } from '../title/titleSelectors';
+import { getSubscriptionColorForNarrow } from '../subscriptions/subscriptionSelectors';
 
 type SelectorProps = $ReadOnly<{|
   titleBackgroundColor: string,
@@ -55,5 +56,7 @@ class ChatScreen extends PureComponent<Props> {
 }
 
 export default connect<SelectorProps, _, _>((state, props) => ({
-  titleBackgroundColor: getTitleBackgroundColor(state, props.navigation.state.params.narrow),
+  titleBackgroundColor:
+    getSubscriptionColorForNarrow(state, props.navigation.state.params.narrow)
+    ?? DEFAULT_TITLE_BACKGROUND_COLOR,
 }))(ChatScreen);
