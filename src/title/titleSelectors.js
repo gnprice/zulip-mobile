c6d6cc977aa04ffffb90b3e79121c5e98d3bfe9a
Greg Price <greg@zulipchat.com>
2020-02-18 17:13:19 -0800

    wip getSub..ColorForName

--- src/title/titleSelectors.js
+++ src/title/titleSelectors.js
@@ -1,7 +1,7 @@
 /* @flow strict-local */
 import type { Narrow, GlobalState } from '../types';
 import { tryStreamNameOfNarrow } from '../utils/narrow';
-import { getSubscriptionsByName } from '../subscriptions/subscriptionSelectors';
+import { getSubscriptionColorForName } from '../subscriptions/subscriptionSelectors';
 
 export const DEFAULT_TITLE_BACKGROUND_COLOR = 'transparent';
 
@@ -12,10 +12,9 @@ export const DEFAULT_TITLE_BACKGROUND_COLOR = 'transparent';
  * Otherwise, it takes a default value.
  */
 export const getTitleBackgroundColor = (state: GlobalState, narrow?: Narrow) => {
-  const subscriptionsByName = getSubscriptionsByName(state);
   const streamName = tryStreamNameOfNarrow(narrow);
   if (streamName === null) {
     return DEFAULT_TITLE_BACKGROUND_COLOR;
   }
-  return subscriptionsByName.get(streamName)?.color ?? 'gray';
+  return getSubscriptionColorForName(state, streamName);
 };
