0abc4069c19ee86eae32c64105bf55c5e3b167d2
Greg Price <greg@zulipchat.com>
2020-02-18 17:16:06 -0800

    wip getSub..ColorForNarrow

--- src/title/titleSelectors.js
+++ src/title/titleSelectors.js
@@ -1,7 +1,6 @@
 /* @flow strict-local */
 import type { Narrow, GlobalState } from '../types';
-import { tryStreamNameOfNarrow } from '../utils/narrow';
-import { getSubscriptionColorForName } from '../subscriptions/subscriptionSelectors';
+import { getSubscriptionColorForNarrow } from '../subscriptions/subscriptionSelectors';
 
 export const DEFAULT_TITLE_BACKGROUND_COLOR = 'transparent';
 
@@ -11,10 +10,5 @@ export const DEFAULT_TITLE_BACKGROUND_COLOR = 'transparent';
  * If `narrow` is a stream or topic narrow, this is based on the stream color.
  * Otherwise, it takes a default value.
  */
-export const getTitleBackgroundColor = (state: GlobalState, narrow?: Narrow) => {
-  const streamName = tryStreamNameOfNarrow(narrow);
-  if (streamName === null) {
-    return DEFAULT_TITLE_BACKGROUND_COLOR;
-  }
-  return getSubscriptionColorForName(state, streamName);
-};
+export const getTitleBackgroundColor = (state: GlobalState, narrow: Narrow) =>
+  getSubscriptionColorForNarrow(state, narrow) ?? DEFAULT_TITLE_BACKGROUND_COLOR;
