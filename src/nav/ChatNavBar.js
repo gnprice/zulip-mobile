ce953d362d1fefc93726ed021428b13d16689cbf
Greg Price <greg@zulipchat.com>
2020-02-18 16:57:58 -0800

    wip ChatNavBar... not sure this is actually better yet

--- src/nav/ChatNavBar.js
+++ src/nav/ChatNavBar.js
@@ -3,18 +3,19 @@
 import React, { PureComponent } from 'react';
 import { View } from 'react-native';
 
-import type { Dispatch, Narrow } from '../types';
+import type { Dispatch, Narrow, Subscription } from '../types';
 import { connect } from '../react-redux';
 import styles, { BRAND_COLOR } from '../styles';
 import Title from '../title/Title';
 import NavButton from './NavButton';
-import { DEFAULT_TITLE_BACKGROUND_COLOR, getTitleBackgroundColor } from '../title/titleSelectors';
 import { foregroundColorFromBackground } from '../utils/color';
 import { navigateBack } from '../actions';
 import { ExtraButton, InfoButton } from '../title-buttons/titleButtonFromNarrow';
+import { getSubscriptionsByName } from '../subscriptions/subscriptionSelectors';
+import { tryStreamNameOfNarrow } from '../utils/narrow';
 
 type SelectorProps = {|
-  backgroundColor: string,
+  subscriptionsByName: Map<string, Subscription>,
 |};
 
 type Props = $ReadOnly<{|
@@ -26,9 +27,12 @@ type Props = $ReadOnly<{|
 
 class ChatNavBar extends PureComponent<Props> {
   render() {
-    const { dispatch, backgroundColor, narrow } = this.props;
+    const { dispatch, subscriptionsByName, narrow } = this.props;
+    const streamName = tryStreamNameOfNarrow(narrow);
+    const backgroundColor =
+      streamName === null ? 'transparent' : subscriptionsByName.get(streamName)?.color ?? 'gray';
     const color =
-      backgroundColor === DEFAULT_TITLE_BACKGROUND_COLOR
+      backgroundColor === 'transparent'
         ? BRAND_COLOR
         : foregroundColorFromBackground(backgroundColor);
 
@@ -50,5 +54,5 @@ class ChatNavBar extends PureComponent<Props> {
 }
 
 export default connect<SelectorProps, _, _>((state, props) => ({
-  backgroundColor: getTitleBackgroundColor(state, props.narrow),
+  subscriptionsByName: getSubscriptionsByName(state),
 }))(ChatNavBar);
