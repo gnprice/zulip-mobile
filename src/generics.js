ab2b399959d14380229f6175123e785cd5dd383b
Greg Price <greg@zulipchat.com>
2020-05-07 15:42:45 -0700

    ro propsminus -- another manifestation of flow 6225

--- src/generics.js
+++ src/generics.js
@@ -117,7 +117,7 @@ export type BoundedDiff<-U, -L> =
     >,
   >;
 
-export type PropsMinus<C, -SP> = BoundedDiff<$Exact<ElementConfig<C>>, SP>;
+export type PropsMinus<C, -SP> = $ReadOnly<BoundedDiff<$Exact<ElementConfig<C>>, SP>>;
 
 /**
  * The object type `T` with its readonly annotation stripped.
