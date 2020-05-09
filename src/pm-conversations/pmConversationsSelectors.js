b9f71a277699f1a7ed042db35d817005e9d22ced
Ray Kraesig <rkraesig@zulip.com>
2020-05-08 19:43:47 -0700

    selectors [nfc]: Extract fragments of `getRecentConversations`.
    We're about to add an alternate implementation of this function, but
    we'll need to keep the existing version to avoid loss of functionality
    on pre-2.1 servers.
    
    Preemptively factor some fragments of the existing implementation out,
    as we'll need to use them in more and/or different places.
    
    [greg: rebased; erased some type annotations that were no longer true,
     where Flow can infer the right types just fine]

--- src/pm-conversations/pmConversationsSelectors.js
+++ src/pm-conversations/pmConversationsSelectors.js
@@ -7,41 +7,45 @@ import { getAllUsersById, getOwnUser } from '../users/userSelectors';
 import { getUnreadByPms, getUnreadByHuddles } from '../unread/unreadSelectors';
 import { pmUnreadsKeyFromMessage, pmKeyRecipientUsersFromMessage } from '../utils/recipient';
 
-export const getRecentConversations: Selector<PmConversationData[]> = createSelector(
-  getOwnUser,
-  getPrivateMessages,
+/**
+ * Given a list of PmConversationPartial or PmConversationData, trim it down to
+ * contain only the most recent message from any conversation, and return them
+ * sorted by recency.
+ */
+const collateByRecipient = <T: { unreadsKey: string, msgId: number }>(
+  items: $ReadOnlyArray<T>,
+): T[] => {
+  const latestByRecipients = new Map();
+  items.forEach(item => {
+    const prev = latestByRecipients.get(item.unreadsKey);
+    if (!prev || item.msgId > prev.msgId) {
+      latestByRecipients.set(item.unreadsKey, item);
+    }
+  });
+
+  const sortedByMostRecent = Array.from(latestByRecipients.values()).sort(
+    (a, b) => +b.msgId - +a.msgId,
+  );
+
+  return sortedByMostRecent;
+};
+
+// (Overtaken by rebase.)
+// type PmConversationPartial = $Diff<PmConversationData, {| unread: mixed |}>;
+// type UnreadAttacher = ($ReadOnlyArray<PmConversationPartial>) => PmConversationData[];
+
+/**
+ * Auxiliary function: fragment of the selector(s) for PmConversationData.
+ * Transforms PmConversationPartial[] to PmConversationData[].
+ *
+ * Note that, since this will only ever be called by other selectors, the inner
+ * function doesn't need to do any memoization of its own.
+ */
+const getAttachUnread = createSelector(
   getUnreadByPms,
   getUnreadByHuddles,
-  getAllUsersById,
-  (
-    ownUser: User,
-    messages: Message[],
-    unreadPms: { [number]: number },
-    unreadHuddles: { [string]: number },
-    allUsersById,
-  ): PmConversationData[] => {
-    const items = messages
-      .map(msg => {
-        // Note this can be a different set of users from those in `keyRecipients`.
-        const unreadsKey = pmUnreadsKeyFromMessage(msg, ownUser.user_id);
-        const keyRecipients = pmKeyRecipientUsersFromMessage(msg, allUsersById, ownUser.user_id);
-        return keyRecipients === null ? null : { unreadsKey, keyRecipients, msgId: msg.id };
-      })
-      .filter(Boolean);
-
-    const latestByRecipients = new Map();
-    items.forEach(item => {
-      const prev = latestByRecipients.get(item.unreadsKey);
-      if (!prev || item.msgId > prev.msgId) {
-        latestByRecipients.set(item.unreadsKey, item);
-      }
-    });
-
-    const sortedByMostRecent = Array.from(latestByRecipients.values()).sort(
-      (a, b) => +b.msgId - +a.msgId,
-    );
-
-    return sortedByMostRecent.map(conversation => ({
+  (unreadPms: { [number]: number }, unreadHuddles: { [string]: number }) => partials =>
+    partials.map(conversation => ({
       key: conversation.unreadsKey,
       keyRecipients: conversation.keyRecipients,
       msgId: conversation.msgId,
@@ -54,7 +58,27 @@ export const getRecentConversations: Selector<PmConversationData[]> = createSele
          end up converted to strings, so this access with string keys works.  We should probably use
          a Map for this and similar maps. */
         unreadPms[conversation.unreadsKey] || unreadHuddles[conversation.unreadsKey],
-    }));
+    })),
+);
+
+export const getRecentConversations: Selector<PmConversationData[]> = createSelector(
+  getOwnUser,
+  getPrivateMessages,
+  getAttachUnread,
+  getAllUsersById,
+  (ownUser: User, messages: Message[], attachUnread, allUsersById): PmConversationData[] => {
+    const items = messages
+      .map(msg => {
+        // Note this can be a different set of users from those in `keyRecipients`.
+        const unreadsKey = pmUnreadsKeyFromMessage(msg, ownUser.user_id);
+        const keyRecipients = pmKeyRecipientUsersFromMessage(msg, allUsersById, ownUser.user_id);
+        return keyRecipients === null ? null : { unreadsKey, keyRecipients, msgId: msg.id };
+      })
+      .filter(Boolean);
+
+    const sortedByMostRecent = collateByRecipient(items);
+
+    return attachUnread(sortedByMostRecent);
   },
 );
 
