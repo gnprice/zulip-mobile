e90eb274a07fb731fbbeb0eae544d77f90ffa03b
Chris Bobbe <cbobbe@zulip.com>
2020-08-03 16:31:45 -0700

    UserAvatarWithPresence [nfc]: Remove unused `realm` prop.
    We stopped needing this in a recent commit where this component
    started taking an AvatarURL instance for the `avatarUrl` prop.

--- src/common/UserAvatarWithPresence.js
+++ src/common/UserAvatarWithPresence.js
@@ -2,10 +2,7 @@
 
 import React, { PureComponent } from 'react';
 
-import type { Dispatch } from '../types';
 import { createStyleSheet } from '../styles';
-import { connect } from '../react-redux';
-import { getCurrentRealm } from '../selectors';
 import UserAvatar from './UserAvatar';
 import PresenceStatusIndicator from './PresenceStatusIndicator';
 import type { AvatarURL } from '../utils/avatar';
@@ -19,11 +16,9 @@ const styles = createStyleSheet({
 });
 
 type Props = $ReadOnly<{|
-  dispatch: Dispatch,
   avatarUrl: AvatarURL,
   email: string,
   size: number,
-  realm: URL,
   shape: 'rounded' | 'square',
   onPress?: () => void,
 |}>;
@@ -34,11 +29,10 @@ type Props = $ReadOnly<{|
  * @prop [avatarUrl] - Absolute or relative url to an avatar image.
  * @prop [email] - User's' email address, to calculate Gravatar URL if not given `avatarUrl`.
  * @prop [size] - Sets width and height in pixels.
- * @prop [realm] - Current realm url, used if avatarUrl is relative.
  * @prop [shape] - 'rounded' (default) means a square with rounded corners.
  * @prop [onPress] - Event fired on pressing the component.
  */
-class UserAvatarWithPresence extends PureComponent<Props> {
+export default class UserAvatarWithPresence extends PureComponent<Props> {
   static defaultProps = {
     email: '',
     size: 32,
@@ -55,7 +49,3 @@ class UserAvatarWithPresence extends PureComponent<Props> {
     );
   }
 }
-
-export default connect(state => ({
-  realm: getCurrentRealm(state),
-}))(UserAvatarWithPresence);
