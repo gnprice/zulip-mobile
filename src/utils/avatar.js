040587cbcc973d45f8a0a7394eaedf5eab10a112
Chris Bobbe <cbobbe@zulip.com>
2020-08-03 16:34:32 -0700

    avatar: Remove unused helper functions.
    It was confusing to rely on these instead of putting avatar URLs
    into a standard form at the edge.
    
    Now, they have no call sites, so they can disappear! :)

--- src/utils/avatar.js
+++ src/utils/avatar.js
@@ -236,36 +236,3 @@ export class UploadedAvatarURL extends AvatarURL {
     return result;
   }
 }
-
-/**
- * When selecting the size of a gravatar we can pick any arbitrary
- * size we want. For server-uploaded avatars this is not the case.
- * We have only:
- *  * default - image is 100x100
- *  * medium - image is 500x500
- *
- * This function converts a normal avatar to medium-sized one.
- */
-export const getMediumAvatar = (avatarUrl: string): string => {
-  const matches = new RegExp(/(\w+)\.png/g).exec(avatarUrl);
-
-  return matches ? avatarUrl.replace(matches[0], `${matches[1]}-medium.png`) : avatarUrl;
-};
-
-export const getGravatarFromEmail = (email: string = '', size: number): string =>
-  `https://secure.gravatar.com/avatar/${md5(email.toLowerCase())}?d=identicon&s=${size}`;
-
-export const getAvatarUrl = (
-  avatarUrl: ?string,
-  email: string,
-  realm: URL,
-  size: number = 80,
-): string => {
-  if (typeof avatarUrl !== 'string') {
-    return getGravatarFromEmail(email, size);
-  }
-
-  const fullUrl = new URL(avatarUrl, realm).toString();
-
-  return size > 100 ? getMediumAvatar(fullUrl) : fullUrl;
-};
