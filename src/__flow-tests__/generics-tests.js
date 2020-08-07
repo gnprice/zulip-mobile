956bf226d8e22aeea3e1215605d33700ea64a9af
Greg Price <greg@zulipchat.com>
2020-08-06 20:27:45 -0700

    wip revise IsSupertype tests a bit
    TODO for branch: take more from me/bounded-diff-tests (f9bb213c1)

--- src/__flow-tests__/generics-tests.js
+++ src/__flow-tests__/generics-tests.js
@@ -1,7 +1,9 @@
 /* @flow strict-local */
 import type { IsSupertype } from '../types';
 
-const magic: empty = (1: $FlowFixMe);
+/* eslint-disable flowtype/space-after-type-colon */
+
+declare var magic: empty;
 
 //
 //
@@ -16,6 +18,11 @@ const magic: empty = (1: $FlowFixMe);
 // We'll need the type we cast from to also be a more interesting type than
 // `empty`, as a value of type `empty` causes Flow to just declare victory:
 (magic: IsSupertype<number, mixed>);
+// This is OK in practice because in real life there are no values of type
+// `empty`; if you have an expression of type `empty`, you're already
+// asserting falsehood.  (And it's perfectly OK from a theoretical
+// perspective for basically the same reason said differently: `empty` is
+// the type interpretation of logical falsity, and ex falso quodlibet.)
 
 // Test the basics, relative to a primitive type.
 (1: IsSupertype<number, empty>);
@@ -33,10 +40,25 @@ const magic: empty = (1: $FlowFixMe);
 // $FlowExpectedError
 ('': IsSupertype<number, number>);
 
-// OTOH if the supertype is `empty`, there's no way to get an error.
-// That's OK because the only way to use the type is to hand it an
-// impossible value anyway:
+// `empty` isn't a supertype of anything but itself.  It's a bit hard to
+// test this fact directly, because of the above-mentioned issue that
+// casting a value of type `empty` causes Flow to just declare victory:
 (magic: IsSupertype<empty, number>);
+// but, again, in reality there are no values of type `empty` anyway.
+// If we try casting from another type, we get an error as you'd hope:
+// $FlowExpectedError
+(1: IsSupertype<empty, number>);
+// and in fact two of them-- one for the cast because nothing can be cast
+// to `empty`, and one for instantiating IsSupertype in the first place.
+// prettier-ignore
+( // $FlowExpectedError
+  1: // $FlowExpectedError
+  IsSupertype<empty, number>);
+
+// Test on unions.
+(1: IsSupertype<number | void, number>);
+// $FlowExpectedError
+(1: IsSupertype<number, number | void>);
 
 // Test on object types.
 {
@@ -55,10 +77,12 @@ const magic: empty = (1: $FlowFixMe);
   // $FlowExpectedError
   (a: IsSupertype<{| a: number |}, { a: number, ... }>);
 
-  // Variance and invariance of properties.
+  // Read-only properties are covariant.
   (a: IsSupertype<{| +a: number |}, {| +a: empty |}>);
   // $FlowExpectedError
   (a: IsSupertype<{| +a: number |}, {| +a: mixed |}>);
+
+  // Read-write properties are invariant.
   // $FlowExpectedError
   (a: IsSupertype<{| a: number |}, {| a: empty |}>);
   // $FlowExpectedError
