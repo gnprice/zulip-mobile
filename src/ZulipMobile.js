9645a7e25a21d8efcda4cadc94ae249a8e80b349
Chris Bobbe <cbobbe@zulip.com>
2020-10-22 09:28:48 -0700

    ZulipMobile: Don't mount most of the component tree until done hydrating.
    See discussion [1] for more on the choice to hide React Navigation's
    `AppContainer` until we're done hydrating, and to do so with
    something like the `LoadingScreen`. We just reuse the
    `LoadingScreen` and adjust its type appropriately.
    
    Here, hide `AppContainer` and more -- all the way up through
    `AppEventHandlers`. It seems good to be able to guarantee to those
    components that, by the time they've mounted, they can have all the
    data from rehydration, if they want it.
    
    A few points on the question of whether this delay in mounting will
    be harmful to any of the logic in these components:
    
    - Their appearance already follows a delay, and a pretty arbitrary
      one at that: React Native has always taken some amount of time
      after the app has launched before it decides it's ready to show
      any of our React components. This isn't always just a few
      milliseconds, either; while debugging, it includes the many
      seconds it takes to download the JS bundle.
    
    - For how we react to an "initial notification" that opened the app,
      it should still work to go and ask for that notification's data
      when we want to ask for it, as we do in
      `handleInitialNotification` in `AppEventHandlers` -- we're not
      constrained by any particular schedule there, as far as I can
      tell. In fact, the real work here is already being postponed until
      after rehydration; this is because `narrowToNotification` is a
      (thunk) action, and so it gets held in a queue by
      redux-action-buffer until we've rehydrated.
    
    - Empirically, I now see our `AppDataFetcher` receiving `true` for
      `needsInitialFetch` in its `componentDidMount`, instead of being
      `false` in `componentDidMount` before `componentDidUpdate` fired
      with it being `true`. In a recent commit, we started handling the
      `true`-in-`componentDidMount` case, so this is fine.
    
    - We attach a few event listeners in `AppEventHandlers` for some
      graphical concerns: handling orientation changes and setting the
      safe-area insets. These should be fine to be delayed as long as
      the component we show during the delay, `LoadingScreen`, doesn't
      depend on these listeners having been set. `LoadingScreen` *does*
      consume `state.session.orientation` and
      `state.session.safeAreaInsets`, which are set from these
      listeners. There's no harm in using the default value of
      "portrait" for orientation, but the default value for the
      safe-area insets isn't useful, so that is a potential concern.
      #4315 would resolve it by not storing the safe-area insets in
      Redux.
    
    - There are a few other listeners that get set. One
      suspicious-looking thing is the `state === 'active'` condition in
      `handleAppStateChange` in `AppEventHandlers`. Are we looking at an
      attempt to set a listener just in time to catch the app's launch
      (to the 'active' state) from being totally closed? No:
      command-clicking through, the instructions in
      `notificationOnAppActive` are meant to be run upon going to the
      active state from the *background* state, not the inactive state.
      So it's fine to delay setting that listener.
    
    [1] https://chat.zulip.org/#narrow/stream/243-mobile-team/topic/React.20Navigation.20v5/near/1039507

--- src/ZulipMobile.js
+++ src/ZulipMobile.js
@@ -3,6 +3,7 @@ import React from 'react';
 import 'react-native-url-polyfill/auto';
 
 import StoreProvider from './boot/StoreProvider';
+import HideIfNotHydrated from './boot/HideIfNotHydrated';
 import TranslationProvider from './boot/TranslationProvider';
 import ThemeProvider from './boot/ThemeProvider';
 import CompatibilityChecker from './boot/CompatibilityChecker';
@@ -14,6 +15,7 @@ import AppContainer from './nav/AppContainer';
 import * as NavigationService from './nav/NavigationService';
 
 import { initializeSentry } from './sentry';
+import LoadingScreen from './start/LoadingScreen';
 
 initializeSentry();
 
@@ -23,19 +25,21 @@ console.disableYellowBox = true; // eslint-disable-line
 export default (): React$Node => (
   <CompatibilityChecker>
     <StoreProvider>
-      <AppEventHandlers>
-        <AppDataFetcher>
-          <TranslationProvider>
-            <ThemeProvider>
-              <InitialNavigationDispatcher>
-                <BackNavigationHandler>
-                  <AppContainer ref={NavigationService.appContainerRef} />
-                </BackNavigationHandler>
-              </InitialNavigationDispatcher>
-            </ThemeProvider>
-          </TranslationProvider>
-        </AppDataFetcher>
-      </AppEventHandlers>
+      <HideIfNotHydrated PlaceholderComponent={LoadingScreen}>
+        <AppEventHandlers>
+          <AppDataFetcher>
+            <TranslationProvider>
+              <ThemeProvider>
+                <InitialNavigationDispatcher>
+                  <BackNavigationHandler>
+                    <AppContainer ref={NavigationService.appContainerRef} />
+                  </BackNavigationHandler>
+                </InitialNavigationDispatcher>
+              </ThemeProvider>
+            </TranslationProvider>
+          </AppDataFetcher>
+        </AppEventHandlers>
+      </HideIfNotHydrated>
     </StoreProvider>
   </CompatibilityChecker>
 );
