5ef60e3cf1d1eebf01d568b1b99809ec3c0a0772
Chris Bobbe <cbobbe@zulip.com>
2021-05-11 12:57:45 -0400

    apiErrors: Add `isServerError`.

--- src/api/__tests__/apiErrors-test.js
+++ src/api/__tests__/apiErrors-test.js
@@ -1,24 +1,34 @@
 /* @flow strict-local */
-import { isClientError, ApiError } from '../apiErrors';
+import { isClientError, ApiError, isServerError } from '../apiErrors';
 
-describe('isClientError', () => {
-  test('an API error with error code between 400 and 499 is a "client error"', () => {
-    const error = new ApiError(404, {
-      code: 'BAD_IMAGE',
-      result: 'error',
-      msg: 'File not found',
-    });
-    const result = isClientError(error);
-    expect(result).toBe(true);
+describe('an API error with error code between 400 and 499', () => {
+  const error = new ApiError(404, {
+    code: 'BAD_IMAGE',
+    result: 'error',
+    msg: 'File not found',
   });
 
-  test('an API error with error code between 500 and 599 is not a "client error"', () => {
-    const error = new ApiError(500, {
-      code: 'BAD_REQUEST',
-      result: 'error',
-      msg: 'Internal server error',
-    });
-    const result = isClientError(error);
-    expect(result).toBe(false);
+  test('is a "client error"', () => {
+    expect(isClientError(error)).toBe(true);
+  });
+
+  test('is not a "server error"', () => {
+    expect(isServerError(error)).toBe(false);
+  });
+});
+
+describe('an API error with error code between 500 and 599', () => {
+  const error = new ApiError(500, {
+    code: 'SOME_ERROR_CODE',
+    msg: 'Internal Server Error',
+    result: 'error',
+  });
+
+  test('is a "server error"', () => {
+    expect(isServerError(error)).toBe(true);
+  });
+
+  test('is not a "client error"', () => {
+    expect(isClientError(error)).toBe(false);
   });
 });
