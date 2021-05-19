088b77883ad5879dcaccd28084bced28af1195e2
Chris Bobbe <cbobbe@zulip.com>
2021-05-19 11:59:23 -0400

    fetchActions: Solidify an assumption and add logging if it's violated.

--- src/message/fetchActions.js
+++ src/message/fetchActions.js
@@ -1,4 +1,5 @@
 /* @flow strict-local */
+import * as logging from '../utils/logging';
 import * as NavigationService from '../nav/NavigationService';
 import type { Narrow, Dispatch, GetState, GlobalState, Message, Action, UserId } from '../types';
 import { ensureUnreachable } from '../types';
@@ -6,7 +7,7 @@ import type { ApiResponseServerSettings } from '../api/settings/getServerSetting
 import type { InitialData } from '../api/initialDataTypes';
 import * as api from '../api';
 import { resetToAccountPicker } from '../actions';
-import { isRetryable } from '../api/apiErrors';
+import { isRetryable, isClientError } from '../api/apiErrors';
 import {
   getAuth,
   getSession,
@@ -386,9 +387,15 @@ export const doInitialFetch = () => async (dispatch: Dispatch, getState: GetStat
       tryFetch(() => api.getServerSettings(auth.realm)),
     ]);
   } catch (e) {
-    // This should only happen on a 4xx HTTP status, which should only
-    // happen when `auth` is no longer valid.  No use retrying; just log out.
-    dispatch(logout());
+    if (isClientError(e)) {
+      // This should only happen when `auth` is no longer valid. No
+      // use retrying; just log out.
+      dispatch(logout());
+    } else {
+      logging.warn(e, {
+        message: 'Unexpected error during initial fetch and serverSettings fetch.',
+      });
+    }
     return;
   }
 
