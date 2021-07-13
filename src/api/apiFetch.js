921a29ceb38e1d5faf8dcb0bd4b7f21b274ad984
Greg Price <greg@zulipchat.com>
2021-07-13 16:56:58 -0700

    api errors [nfc]: Put success and failure logic together.
    This gives us a better shot at seeing all in one place how we
    interpret the server's response and what the possible cases are.

--- src/api/apiFetch.js
+++ src/api/apiFetch.js
@@ -6,7 +6,7 @@ import { getAuthHeaders } from './transport';
 import { encodeParamsForUrl } from '../utils/url';
 import userAgent from '../utils/userAgent';
 import { networkActivityStart, networkActivityStop } from '../utils/networkActivity';
-import { makeErrorFromApi, MalformedResponseError, RequestError } from './apiErrors';
+import { interpretApiResponse, MalformedResponseError, RequestError } from './apiErrors';
 import * as logging from '../utils/logging';
 
 const apiVersion = 'api/v1';
@@ -47,10 +47,7 @@ export const apiCall = async (
     networkActivityStart(isSilent);
     const response = await apiFetch(auth, route, params);
     const json = await response.json().catch(() => undefined);
-    if (response.status >= 200 && response.status <= 299 && json !== undefined) {
-      return json;
-    }
-    throw makeErrorFromApi(response.status, json);
+    return interpretApiResponse(response.status, json);
   } catch (errorIllTyped) {
     const error: mixed = errorIllTyped; // https://github.com/facebook/flow/issues/2470
 
