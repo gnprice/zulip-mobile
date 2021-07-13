aa7c71fa45ef83bc747ccb955354c628b427f75f
Greg Price <greg@zulipchat.com>
2021-07-13 16:00:42 -0700

    api errors [nfc]: Simplify logic in the well-formed case.

--- src/api/apiErrors.js
+++ src/api/apiErrors.js
@@ -57,20 +57,11 @@ export const makeErrorFromApi = (httpStatus: number, data: mixed): Error => {
     return new Server5xxError(httpStatus);
   }
 
-  // Validate `data`, and construct the resultant error object.
   if (typeof data === 'object' && data !== null) {
-    const { result, msg, code } = data;
-    if (result === 'error' && typeof msg === 'string') {
-      // If `code` is present, it must be a string.
-      if (code === undefined || typeof code === 'string') {
-        // Default to 'BAD_REQUEST' if `code` is not present.
-        return new ApiError(httpStatus, {
-          ...data,
-          result,
-          msg,
-          code: code ?? 'BAD_REQUEST',
-        });
-      }
+    const { result, msg, code = 'BAD_REQUEST' } = data;
+    if (result === 'error' && typeof msg === 'string' && typeof code === 'string') {
+      // Hooray, we have a well-formed Zulip API error blob.  Use that.
+      return new ApiError(httpStatus, { ...data, result, msg, code });
     }
   }
 
