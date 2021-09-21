2919bd6a68edd631357f0b936fbd8e78c8a67fce
Greg Price <greg@zulipchat.com>
2021-09-20 17:51:59 -0700

    wip partially type; based on upstream at same version

--- src/third/redux-thunk/index.js
+++ src/third/redux-thunk/index.js
@@ -1,14 +1,43 @@
-function createThunkMiddleware(extraArgument) {
-  return ({ dispatch, getState }) => next => action => {
-    if (typeof action === 'function') {
-      return action(dispatch, getState, extraArgument);
-    }
+/* @flow strict-local */
 
-    return next(action);
-  };
+/* eslint-disable no-use-before-define */
+
+import type { Action as GenericAction, Middleware } from 'redux';
+
+type Action = GenericAction<mixed>;
+
+export interface ThunkDispatch<S, E, A: Action> {
+  <T: A>(action: T): T;
+  <R>(asyncAction: ThunkAction<R, S, E, A>): R;
+}
+
+export type ThunkAction<R, S, E, A: Action> = (
+  dispatch: ThunkDispatch<S, E, A>,
+  getState: () => S,
+  extraArgument: E,
+) => R;
+
+export type ThunkMiddleware<S = {}, A: Action = Action, E = void> = Middleware<
+  S,
+  A,
+  ThunkDispatch<S, E, A>,
+>;
+
+function createThunkMiddleware<S = {}, A: Action = Action, E = void>(
+  extraArgument: E,
+): ThunkMiddleware<S, A, E> {
+  return ({ dispatch, getState }) => (next: ThunkDispatch<S, E, A>): ThunkDispatch<S, E, A> =>
+    // $FlowFixMe[incompatible-return]: overload seems tricky; also returning action is silly
+    function inner<R>(action: A | ThunkAction<R, S, E, A>): R {
+      if (typeof action === 'function') {
+        return action(dispatch, getState, extraArgument);
+      }
+
+      return (next(action): $FlowFixMe); // returning the action is silly
+    };
 }
 
 const thunk = createThunkMiddleware();
-thunk.withExtraArgument = createThunkMiddleware;
+// thunk.withExtraArgument = createThunkMiddleware;
 
-export default thunk;
+export default (thunk: mixed);
