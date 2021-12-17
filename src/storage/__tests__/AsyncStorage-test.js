01b7d631090431f486d16f0d86bad1fae99c1232
Greg Price <greg@zulipchat.com>
2021-12-16 22:02:54 -0800

    wip AsyncStorage: rework migration structure

--- src/storage/__tests__/AsyncStorage-test.js
+++ src/storage/__tests__/AsyncStorage-test.js
@@ -106,7 +106,7 @@ describe('AsyncStorage: migration from legacy AsyncStorage', () => {
     const db = new SQLDatabase('zulip.db');
     await db.transaction(tx => {
       // … and that the migration didn't record success.
-      tx.executeSql('DELETE FROM migrations');
+      tx.executeSql('DELETE FROM migration');
     });
 
     // Expect to get the original legacy data, re-migrated.
