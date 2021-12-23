5588a5af05f523239abb37f4db22013b68c0ea32
Greg Price <greg@zulipchat.com>
2021-12-23 12:57:41 -0800

    Revert "WIP attempt to handle mid-txn failure; partial success?"

--- src/storage/sqlite.js
+++ src/storage/sqlite.js
@@ -29,7 +29,11 @@ export class SQLDatabase {
   transaction(cb: SQLTransaction => void | Promise<void>): Promise<void> {
     return new Promise((resolve, reject) =>
       this.db.transaction(
-        tx => void keepQueueLiveWhile(tx, () => cb(new SQLTransactionImpl(this, tx))),
+        tx =>
+          void keepQueueLiveWhile(tx, () => cb(new SQLTransactionImpl(this, tx))).catch(err =>
+            // Well, at least we can ensure the outer Promise rejects.
+            reject(err),
+          ),
         reject,
         resolve,
       ),
@@ -39,7 +43,11 @@ export class SQLDatabase {
   readTransaction(cb: SQLTransaction => void | Promise<void>): Promise<void> {
     return new Promise((resolve, reject) =>
       this.db.readTransaction(
-        tx => void keepQueueLiveWhile(tx, () => cb(new SQLTransactionImpl(this, tx))),
+        tx =>
+          void keepQueueLiveWhile(tx, () => cb(new SQLTransactionImpl(this, tx))).catch(err =>
+            // Well, at least we can ensure the outer Promise rejects.
+            reject(err),
+          ),
         reject,
         resolve,
       ),
@@ -76,23 +84,13 @@ async function keepQueueLiveWhile(
   f: () => void | Promise<void>,
 ): Promise<void> {
   let done = false;
-  let err = undefined;
-  const hold = () =>
-    tx.executeSql('SELECT 1', [], () => {
-      if (err) {
-        tx.executeSql('SELECT failed');
-      } else if (!done) {
-        hold();
-      }
-    });
+  const hold = () => tx.executeSql('SELECT 1', [], () => (done ? undefined : hold()));
   hold();
   try {
     await f();
-  } catch (e) {
-    err = e;
-    throw e;
+  } finally {
+    done = true;
   }
-  done = true;
 }
 
 class SQLTransactionImpl {
