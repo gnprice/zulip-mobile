a824d8b4b1d43d909ebc8ba0a0d12069634070e6
Greg Price <greg@zulipchat.com>
2021-12-22 21:23:16 -0800

    wip sqlite: make similar async-test for our version

--- src/storage/__tests__/sqlite-test.js
+++ src/storage/__tests__/sqlite-test.js
@@ -236,4 +236,49 @@ describe('our promisified sqlite', () => {
     expect(a?.rows._array).toEqual([{ n: 1 }]);
     expect(b?.rows._array).toEqual([{ n: 2 }]);
   });
+
+  test('transaction with internal asynchrony other than executeSql', async () => {
+    // This corresponds to the similar test for expo-sqlite above, and
+    // shows that we've successfully worked around that issue.
+
+    let p1 = undefined;
+    let p2 = undefined;
+
+    const db = new SQLDatabase(dbName);
+    const txPromise = db.transaction(async tx => {
+      tx.executeSql('CREATE TABLE foo (name TEXT, value INT)');
+      p1 = tx.executeSql('SELECT 2 + 2 AS total');
+      const result = await p1;
+      const { total } = result.rows._array[0];
+
+      // Use that data to compute something synchronously, and store it.
+      const double = 2 * total;
+      tx.executeSql('INSERT INTO foo (name, value) VALUES (?, ?)', ['double', double]);
+
+      // Now do some *asynchronous* computation, using the data we
+      // read earlier in the transaction.  We'll use the exact same function as above:
+      const asyncSquare = (x, cb) => setTimeout(() => cb(x * x), 0);
+      p2 = new Promise(resolve => asyncSquare(total, resolve));
+      const square = await p2;
+      tx.executeSql('INSERT INTO foo (name, value) VALUES (?, ?)', ['square', square]);
+    });
+
+    // Let that asynchronous computation complete.  This gets a bit ugly: we
+    // need to tell Jest to run the timer embedded inside that async
+    // computation, and that does no good until the timer has been created,
+    // so we end up having to walk through the async flow step by step.
+    // prettier-ignore
+    while (!p1) { await null; }
+    await p1;
+    // prettier-ignore
+    while (!p2) { await null; }
+    jest.runAllTimers();
+
+    await txPromise;
+
+    const rows = await db.query<{ name: string, value: number }>('SELECT name, value FROM foo', []);
+    const data = objectFromEntries(rows.map(({ name, value }) => [name, value]));
+
+    expect(data).toEqual({ double: 8, square: 16 });
+  });
 });
