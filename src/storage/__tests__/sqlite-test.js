3090845dc034776ea8e9a8040c9962ed6a7a1bdf
Greg Price <greg@zulipchat.com>
2021-12-28 16:07:48 -0800

    sqlite tests [nfc]: Simplify with some helpers, to bridge callbacks vs. Jest

--- src/storage/__tests__/sqlite-test.js
+++ src/storage/__tests__/sqlite-test.js
@@ -31,69 +31,48 @@ describe('expo-sqlite', () => {
     await deleteDatabase(dbName);
   });
 
+  // These helpers `promiseTxn`, `promiseRead`, and `select` are convenient
+  // for stitching the database control flow into the control flow of a
+  // test.  They're similar to parts of our promisified wrapper... namely
+  // the outermost, simplest parts.
+  const promiseTxn = (db, cb) =>
+    new Promise((resolve, reject) => db.transaction(cb, reject, resolve));
+
+  const promiseRead = (db, cb) =>
+    new Promise((resolve, reject) => db.readTransaction(cb(resolve), reject, resolve));
+
+  const select = (db, sql, args = []) =>
+    promiseRead(db, resolve => tx => tx.executeSql(sql, args, (t, r) => resolve(r))).then(
+      r => r.rows._array,
+    );
+
   test('smoke', async () => {
     const db = openDatabase(dbName);
-    const result = await new Promise((resolve, reject) => {
-      db.readTransaction(
-        tx => {
-          tx.executeSql('SELECT 42 AS n', [], (t, r) => resolve(r));
-        },
-        reject,
-        resolve,
-      );
-    });
-    expect(result.rows._array).toEqual([{ n: 42 }]);
+    const rows = await select(db, 'SELECT 42 AS n');
+    expect(rows).toEqual([{ n: 42 }]);
   });
 
   test('transaction with no internal await', async () => {
     const db = openDatabase(dbName);
-    await new Promise((resolve, reject) =>
-      db.transaction(
-        tx => {
-          tx.executeSql('CREATE TABLE foo (x INT)');
-          tx.executeSql('INSERT INTO foo (x) VALUES (?)', [1]);
-          tx.executeSql('INSERT INTO foo (x) VALUES (?)', [2]);
-        },
-        reject,
-        resolve,
-      ),
-    );
-    const result = await new Promise((resolve, reject) => {
-      db.readTransaction(
-        tx => {
-          tx.executeSql('SELECT x FROM foo', [], (t, r) => resolve(r));
-        },
-        reject,
-        resolve,
-      );
+    await promiseTxn(db, tx => {
+      tx.executeSql('CREATE TABLE foo (x INT)');
+      tx.executeSql('INSERT INTO foo (x) VALUES (?)', [1]);
+      tx.executeSql('INSERT INTO foo (x) VALUES (?)', [2]);
     });
-    expect(result.rows._array).toEqual([{ x: 1 }, { x: 2 }]);
+    const rows = await select(db, 'SELECT x FROM foo');
+    expect(rows).toEqual([{ x: 1 }, { x: 2 }]);
   });
 
   test('transaction with internal await^W callback ', async () => {
     const db = openDatabase(dbName);
-    await new Promise((resolve, reject) =>
-      db.transaction(
-        tx => {
-          tx.executeSql('CREATE TABLE foo (x INT)');
-          tx.executeSql('INSERT INTO foo (x) VALUES (?)', [1], () =>
-            tx.executeSql('INSERT INTO foo (x) VALUES (?)', [2]),
-          );
-        },
-        reject,
-        resolve,
-      ),
-    );
-    const result = await new Promise((resolve, reject) => {
-      db.readTransaction(
-        tx => {
-          tx.executeSql('SELECT x FROM foo', [], (t, r) => resolve(r));
-        },
-        reject,
-        resolve,
+    await promiseTxn(db, tx => {
+      tx.executeSql('CREATE TABLE foo (x INT)');
+      tx.executeSql('INSERT INTO foo (x) VALUES (?)', [1], () =>
+        tx.executeSql('INSERT INTO foo (x) VALUES (?)', [2]),
       );
     });
-    expect(result.rows._array).toEqual([{ x: 1 }, { x: 2 }]);
+    const rows = await select(db, 'SELECT x FROM foo');
+    expect(rows).toEqual([{ x: 1 }, { x: 2 }]);
   });
 
   test('BROKEN: transaction with internal asynchrony other than executeSql', async () => {
@@ -113,41 +92,35 @@ describe('expo-sqlite', () => {
 
     const db = openDatabase(dbName);
     let txEnded = false;
-    await new Promise((resolve, reject) =>
-      db.transaction(
-        tx => {
-          tx.executeSql('CREATE TABLE foo (name TEXT, value INT)');
+    await promiseTxn(db, tx => {
+      tx.executeSql('CREATE TABLE foo (name TEXT, value INT)');
 
-          // Get some data from SQL.
-          tx.executeSql('SELECT 2 + 2 AS total', [], (t, result) => {
-            const { total } = result.rows._array[0];
+      // Get some data from SQL.
+      tx.executeSql('SELECT 2 + 2 AS total', [], (t, result) => {
+        const { total } = result.rows._array[0];
 
-            // Use that data to compute something synchronously, and store it.
-            const double = 2 * total;
-            tx.executeSql('INSERT INTO foo (name, value) VALUES (?, ?)', ['double', double]);
-            // So far, all's well.
+        // Use that data to compute something synchronously, and store it.
+        const double = 2 * total;
+        tx.executeSql('INSERT INTO foo (name, value) VALUES (?, ?)', ['double', double]);
+        // So far, all's well.
 
-            // Now do some *asynchronous* computation, using the data we
-            // read earlier in the transaction.
-            const asyncSquare = (x, cb) => setTimeout(() => cb(x * x), 0);
-            asyncSquare(total, square => {
-              // By the time we get here, the transaction will have found its
-              // queue empty and decided it's complete.
+        // Now do some *asynchronous* computation, using the data we
+        // read earlier in the transaction.
+        const asyncSquare = (x, cb) => setTimeout(() => cb(x * x), 0);
+        asyncSquare(total, square => {
+          // By the time we get here, the transaction will have found its
+          // queue empty and decided it's complete.
 
-              // So when we try to write this result, nothing will happen.
-              tx.executeSql('INSERT INTO foo (name, value) VALUES (?, ?)', ['square', square]);
+          // So when we try to write this result, nothing will happen.
+          tx.executeSql('INSERT INTO foo (name, value) VALUES (?, ?)', ['square', square]);
 
-              // The new statement goes into a queue which will never again
-              // get read.  We don't even get an exception -- the next line
-              // runs fine (as we confirm below):
-              txEnded = true;
-            });
-          });
-        },
-        reject,
-        resolve,
-      ),
-    );
+          // The new statement goes into a queue which will never again
+          // get read.  We don't even get an exception -- the next line
+          // runs fine (as we confirm below):
+          txEnded = true;
+        });
+      });
+    });
 
     // Let that asynchronous computation complete, and confirm the callback
     // completes without an exception.
@@ -157,14 +130,8 @@ describe('expo-sqlite', () => {
     }
 
     // Now read what data got written.
-    const result = await new Promise((resolve, reject) => {
-      db.readTransaction(
-        tx => tx.executeSql('SELECT name, value FROM foo', [], (t, r) => resolve(r)),
-        reject,
-        resolve,
-      );
-    });
-    const data = objectFromEntries(result.rows._array.map(({ name, value }) => [name, value]));
+    const rows = await select(db, 'SELECT name, value FROM foo');
+    const data = objectFromEntries(rows.map(({ name, value }) => [name, value]));
 
     // This would be a good answer:
     // expect(data).toEqual({ double: 8, square: 16 }); // FAILS
