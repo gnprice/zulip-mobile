f06774696a8aacf710c1529de9e8afd8b75832be
Greg Price <greg@zulipchat.com>
2022-02-04 11:49:13 -0800

    more bump

--- android/app/src/main/java/com/zulipmobile/notifications/NotificationChannelManager.kt
+++ android/app/src/main/java/com/zulipmobile/notifications/NotificationChannelManager.kt
@@ -24,7 +24,7 @@ import java.lang.RuntimeException
 
 /** The channel ID we use for our one notification channel, which we use for all notifications. */
 // Previous values: "default", "messages-1"
-val CHANNEL_ID = "messages-2ankeuiv6"
+val CHANNEL_ID = "messages-2ankeuiv7"
 
 /** The vibration pattern we set. */
 // We try to set a vibration pattern that, with the phone in one's pocket,
