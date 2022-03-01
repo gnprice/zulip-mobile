b660aceaf4cc452853cc224738ba7eadf95df953
Greg Price <greg@zulipchat.com>
2022-02-28 21:23:34 -0800

    wip ro GlobalParamList

--- src/nav/globalTypes.js
+++ src/nav/globalTypes.js
@@ -5,9 +5,9 @@ import type { SharingNavigatorParamList } from '../sharing/SharingScreen';
 import type { StreamTabsNavigatorParamList } from '../main/StreamTabsScreen';
 import type { MainTabsNavigatorParamList } from '../main/MainTabsScreen';
 
-export type GlobalParamList = {|
+export type GlobalParamList = $ReadOnly<{|
   ...AppNavigatorParamList,
   ...SharingNavigatorParamList,
   ...StreamTabsNavigatorParamList,
   ...MainTabsNavigatorParamList,
-|};
+|}>;
