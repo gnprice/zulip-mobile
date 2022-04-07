433caf5d6638186c3f8c5564daf716c20fcbe0a3
Greg Price <greg@zulip.com>
2022-04-06 19:11:41 -0700

    wip use ts for recast

--- tools/codemod/flowgen-fix.ts
+++ tools/codemod/flowgen-fix.ts
@@ -15,32 +15,25 @@
  *    the technique used here -- it ends up being super simple, thankfully:
  *      https://github.com/flowtype/flow-codemod/tree/master/transforms/strict-type-args
  *
- * @flow strict-local
  */
 
-// $FlowFixMe[untyped-import]
 import * as recast from 'recast';
-// $FlowFixMe[untyped-import]
 import * as flowParser from 'recast/parsers/flow';
-// $FlowFixMe[untyped-import]
 import { builders as b, namedTypes as n } from 'ast-types';
-// $FlowFixMe[untyped-import]
 import { NodePath } from 'ast-types/lib/node-path';
 import assert from 'assert';
 
 /* eslint-disable no-cond-assign */
 /* eslint-disable consistent-return */
 /* eslint-disable no-console */
+/* eslint-disable flowtype/no-types-missing-file-annotation */
 
 export const parser = 'flow';
 
-// For the Flow `%checks` syntax, see:
-//   https://flow.org/en/docs/types/functions/#toc-predicate-functions
-const checkStatement = (node: n.Node): boolean %checks => n.Statement.check(node);
+// const checkStatement = (node: n.Node): boolean => n.Statement.check(node);
 
 export default function (fileInfo: any, { jscodeshift: j, report }: any) {
-  // Adapted from zulip/zulip@02511bff1: use jscodeshift for its CLI, tweak a
-  //   few bits of syntax for Flow, and apply to Array#forEach instead of _.each.
+  // Adapted loosely from zulip/zulip@02511bff1.
 
   const ast = recast.parse(fileInfo.source, { parser: flowParser });
 
