054fbeab675f6578fb174e83d2c7eace2f0d9715
Greg Price <greg@zulip.com>
2022-06-16 21:37:22 -0700

    wip nav suppress warning about non-serializable avatar URL at LightboxScreen

--- src/lightbox/LightboxScreen.js
+++ src/lightbox/LightboxScreen.js
@@ -1,7 +1,7 @@
 /* @flow strict-local */
 import React from 'react';
 import type { Node } from 'react';
-import { View } from 'react-native';
+import { LogBox, View } from 'react-native';
 
 import type { Message } from '../types';
 import type { RouteProp } from '../react-navigation';
@@ -22,6 +22,19 @@ type Props = $ReadOnly<{|
   navigation: AppNavigationProp<'lightbox'>,
   route: RouteProp<'lightbox', {| src: string, message: Message |}>,
 |}>;
+
+// React Navigation would give us a console warning about non-serializable
+// route params. For more about the warning, see
+//   https://reactnavigation.org/docs/5.x/troubleshooting/#i-get-the-warning-non-serializable-values-were-found-in-the-navigation-state
+//
+// In general, we don't persist navigation state, so it's fine that there's
+// an AvatarURL instance in this screen's route params.  If we wanted to
+// make it serializable, and so compatible with persisting nav state, we
+// could do so by taking only a message ID and looking up the actual message
+// in Redux.
+LogBox.ignoreLogs([
+  /^Non-serializable values were found in the navigation state\..*\nlightbox > \S+\.avatar_url\b/s,
+]);
 
 export default function LightboxScreen(props: Props): Node {
   const { src, message } = props.route.params;
