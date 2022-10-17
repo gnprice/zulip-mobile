gnprice/zulip-mobile

commit fc06c946603f90d5ae26e6438c250852751c719b

nav layout: Have main-tabs screens consume top inset individually
Instead of consuming it in their shared parent component,
MainTabsScreen.

Now, when we add app bars on each of these screens -- which, oops,
we did for SubscriptionsScreen in 9d11e787f without prepping with
this first -- the app-bar component can properly consume the top
inset, covering it with its own appropriate, contentless surface.

In particular, to do so "properly", the top inset must not be
double-consumed by MainTabsScreen; fix that.

So, this fixes a regression where we had twice as much top-padding
as we needed to account for the top inset in the "Streams" screen
(SubscriptionsScreen).

src/account-info/ProfileScreen.js
@@ -2,6 +2,7 @@
 import React, { useContext } from 'react';
 import type { Node } from 'react';
 import { ScrollView, View, Alert } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 import { type UserId } from '../api/idTypes';
 import { TranslationContext } from '../boot/TranslationProvider';
@@ -131,22 +132,24 @@ export default function ProfileScreen(props: Props): Node {
   const ownUser = useSelector(getOwnUser);
 
   return (
-    <ScrollView>
-      <AccountDetails user={ownUser} />
-      <AwayStatusSwitch />
-      <View style={styles.buttonRow}>
-        <SetStatusButton />
-      </View>
-      <View style={styles.buttonRow}>
-        <ProfileButton ownUserId={ownUser.user_id} />
-      </View>
-      <View style={styles.buttonRow}>
-        <SettingsButton />
-      </View>
-      <View style={styles.buttonRow}>
-        <SwitchAccountButton />
-        <LogoutButton />
-      </View>
-    </ScrollView>
+    <SafeAreaView mode="padding" edges={['top']} style={{ flex: 1 }}>
+      <ScrollView>
+        <AccountDetails user={ownUser} />
+        <AwayStatusSwitch />
+        <View style={styles.buttonRow}>
+          <SetStatusButton />
+        </View>
+        <View style={styles.buttonRow}>
+          <ProfileButton ownUserId={ownUser.user_id} />
+        </View>
+        <View style={styles.buttonRow}>
+          <SettingsButton />
+        </View>
+        <View style={styles.buttonRow}>
+          <SwitchAccountButton />
+          <LogoutButton />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }

src/main/HomeScreen.js
@@ -3,6 +3,7 @@
 import React from 'react';
 import type { Node } from 'react';
 import { View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 import type { RouteProp } from '../react-navigation';
 import type { MainTabsNavigationProp } from './MainTabsScreen';
@@ -38,7 +39,7 @@ export default function HomeScreen(props: Props): Node {
   const dispatch = useDispatch();
 
   return (
-    <View style={styles.wrapper}>
+    <SafeAreaView mode="padding" edges={['top']} style={styles.wrapper}>
       <View style={styles.iconList}>
         <TopTabButton
           name="globe"
@@ -70,6 +71,6 @@ export default function HomeScreen(props: Props): Node {
       <ServerPushSetupBanner />
       <LoadingBanner />
       <UnreadCards />
-    </View>
+    </SafeAreaView>
   );
 }

src/main/MainTabsScreen.js
@@ -1,11 +1,11 @@
 /* @flow strict-local */
 import React, { useContext } from 'react';
 import type { Node } from 'react';
+import { View } from 'react-native';
 import {
   createBottomTabNavigator,
   type BottomTabNavigationProp,
 } from '@react-navigation/bottom-tabs';
-import { SafeAreaView } from 'react-native-safe-area-context';
 import type { RouteProp, RouteParamsOf } from '../react-navigation';
 import { getUnreadHuddlesTotal, getUnreadPmsTotal } from '../selectors';
 import { useSelector } from '../react-redux';
@@ -53,7 +53,7 @@ export default function MainTabsScreen(props: Props): Node {
   const unreadPmsCount = useSelector(getUnreadHuddlesTotal) + useSelector(getUnreadPmsTotal);
 
   return (
-    <SafeAreaView mode="padding" edges={['top']} style={[styles.flexed, { backgroundColor }]}>
+    <View style={[styles.flexed, { backgroundColor }]}>
       <OfflineNotice />
       <Tab.Navigator {...bottomTabNavigatorConfig()} lazy={false} backBehavior="none">
         <Tab.Screen
@@ -94,6 +94,6 @@ export default function MainTabsScreen(props: Props): Node {
           }}
         />
       </Tab.Navigator>
-    </SafeAreaView>
+    </View>
   );
 }

src/pm-conversations/PmConversationsScreen.js
@@ -3,6 +3,7 @@
 import React, { useContext } from 'react';
 import type { Node } from 'react';
 import { View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 import type { RouteProp } from '../react-navigation';
 import type { MainTabsNavigationProp } from '../main/MainTabsScreen';
@@ -48,7 +49,11 @@ export default function PmConversationsScreen(props: Props): Node {
   const context = useContext(ThemeContext);
 
   return (
-    <View style={[styles.container, { backgroundColor: context.backgroundColor }]}>
+    <SafeAreaView
+      mode="padding"
+      edges={['top']}
+      style={[styles.container, { backgroundColor: context.backgroundColor }]}
+    >
       <View style={styles.row}>
         <ZulipButton
           secondary
@@ -75,6 +80,6 @@ export default function PmConversationsScreen(props: Props): Node {
       ) : (
         <PmConversationList conversations={conversations} />
       )}
-    </View>
+    </SafeAreaView>
   );
 }

src/streams/SubscriptionsScreen.js
@@ -94,7 +94,9 @@ export default function SubscriptionsScreen(props: Props): Node {
 
   return (
     <View style={styles.container}>
+      {/* Consumes the top inset. */}
       <ModalNavBar canGoBack={false} title="Streams" />
+
       <LoadingBanner />
       {subscriptions.length === 0 ? (
         <SearchEmptyState text="No streams found" />

commit cb93aa37f5e8bf150e7e1fd9e0a4700f26c7d7b6

safe areas: Put OfflineNotice on each tab screen, out of top inset
This fixes a regression introduced in fc06c9466. In that commit, we
asked MainTabsScreen to stop padding the top inset, and asked each
individual tab screen to pad it. With this <OfflineNotice /> being
left behind in MainTabsScreen, it lost its protection from the top
inset, and on my device the crucial "No Internet connection" text
was completely hidden by the notch.

So, remove that single <OfflineNotice /> and add one in each of the
four tab screens, using each screen's protection against rendering
content in the top inset.

src/account-info/ProfileScreen.js
@@ -19,6 +19,7 @@ import { getOwnUser } from '../users/userSelectors';
 import { getIdentity } from '../account/accountsSelectors';
 import { useNavigation } from '../react-navigation';
 import { showConfirmationDialog } from '../utils/info';
+import OfflineNotice from '../common/OfflineNotice';
 
 const styles = createStyleSheet({
   buttonRow: {
@@ -128,6 +129,7 @@ export default function ProfileScreen(props: Props): Node {
 
   return (
     <SafeAreaView mode="padding" edges={['top']} style={{ flex: 1 }}>
+      <OfflineNotice />
       <ScrollView>
         <AccountDetails user={ownUser} />
         <AwayStatusSwitch />

src/main/HomeScreen.js
@@ -17,6 +17,7 @@ import { BRAND_COLOR, createStyleSheet } from '../styles';
 import LoadingBanner from '../common/LoadingBanner';
 import ServerCompatBanner from '../common/ServerCompatBanner';
 import ServerPushSetupBanner from '../common/ServerPushSetupBanner';
+import OfflineNotice from '../common/OfflineNotice';
 
 const styles = createStyleSheet({
   wrapper: {
@@ -40,6 +41,7 @@ export default function HomeScreen(props: Props): Node {
 
   return (
     <SafeAreaView mode="padding" edges={['top']} style={styles.wrapper}>
+      <OfflineNotice />
       <View style={styles.iconList}>
         <TopTabButton
           name="globe"

src/main/MainTabsScreen.js
@@ -15,7 +15,6 @@ import HomeScreen from './HomeScreen';
 import PmConversationsScreen from '../pm-conversations/PmConversationsScreen';
 import { IconInbox, IconStream, IconPeople } from '../common/Icons';
 import OwnAvatar from '../common/OwnAvatar';
-import OfflineNotice from '../common/OfflineNotice';
 import ProfileScreen from '../account-info/ProfileScreen';
 import styles, { BRAND_COLOR, ThemeContext } from '../styles';
 import SubscriptionsScreen from '../streams/SubscriptionsScreen';
@@ -54,7 +53,6 @@ export default function MainTabsScreen(props: Props): Node {
 
   return (
     <View style={[styles.flexed, { backgroundColor }]}>
-      <OfflineNotice />
       <Tab.Navigator {...bottomTabNavigatorConfig()} lazy={false} backBehavior="none">
         <Tab.Screen
           name="home"

src/pm-conversations/PmConversationsScreen.js
@@ -15,6 +15,7 @@ import LoadingBanner from '../common/LoadingBanner';
 import { IconPeople, IconPerson } from '../common/Icons';
 import PmConversationList from './PmConversationList';
 import { getRecentConversations } from '../selectors';
+import OfflineNotice from '../common/OfflineNotice';
 
 const styles = createStyleSheet({
   container: {
@@ -54,6 +55,7 @@ export default function PmConversationsScreen(props: Props): Node {
       edges={['top']}
       style={[styles.container, { backgroundColor: context.backgroundColor }]}
     >
+      <OfflineNotice />
       <View style={styles.row}>
         <ZulipButton
           secondary

src/streams/SubscriptionsScreen.js
@@ -21,6 +21,7 @@ import { caseInsensitiveCompareFunc } from '../utils/misc';
 import StreamItem from './StreamItem';
 import ModalNavBar from '../nav/ModalNavBar';
 import NestedNavRow from '../common/NestedNavRow';
+import OfflineNotice from '../common/OfflineNotice';
 
 const styles = createStyleSheet({
   container: {
@@ -73,6 +74,7 @@ export default function SubscriptionsScreen(props: Props): Node {
     <View style={styles.container}>
       {/* Consumes the top inset. */}
       <ModalNavBar canGoBack={false} title="Streams" />
+      <OfflineNotice />
 
       <LoadingBanner />
       {subscriptions.length === 0 ? (