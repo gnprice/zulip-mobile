dd05d36336da752e238b06c4a29fcc8861f3a083
Greg Price <greg@zulip.com>
2022-12-07 16:52:10 -0800

    WIP start attempting an integration test

---
+++ android/app/src/androidTest/java/com/zulipmobile/StorageTest.kt
@@ -0,0 +1,11 @@
+package com.zulipmobile
+
+import android.test.AndroidTestCase
+//import com.facebook.react.testing.ReactIntegrationTestCase;
+
+//class StorageTest: ReactIntegrationTestCase {
+class StorageTest : AndroidTestCase {
+    fun testSmoke() {
+        assert(false) { "hooray, reached test" }
+    }
+}
