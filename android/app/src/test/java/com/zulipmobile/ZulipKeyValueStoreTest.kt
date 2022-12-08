a76e6ba4606f2f33912a69bcd9a92212c66df9eb
Greg Price <greg@zulip.com>
2022-12-07 16:04:58 -0800

    progress?: base Activity
    Now it gets as far as finding the database file (well, its
    parent directory) doesn't exist:
    
    ```
    Cannot open database '/tmp/robolectric-Method_tmp_test14031462555267843593/com.zulipmobile.debug-dataDir/files/SQLite/zulip.db': Directory /tmp/robolectric-Method_tmp_test14031462555267843593/com.zulipmobile.debug-dataDir/files/SQLite doesn't exist
    android.database.sqlite.SQLiteCantOpenDatabaseException: Cannot open database '/tmp/robolectric-Method_tmp_test14031462555267843593/com.zulipmobile.debug-dataDir/files/SQLite/zulip.db': Directory /tmp/robolectric-Method_tmp_test14031462555267843593/com.zulipmobile.debug-dataDir/files/SQLite doesn't exist
    	at android.database.sqlite.SQLiteConnection.open(SQLiteConnection.java:254)
    	at android.database.sqlite.SQLiteConnection.open(SQLiteConnection.java:205)
    …
    	at android.database.sqlite.SQLiteDatabase.openDatabase(SQLiteDatabase.java:752)
    	at com.zulipmobile.ZulipDbOpenHelper.getDatabaseLocked(AppData.kt:166)
    …
    ```

--- android/app/src/test/java/com/zulipmobile/ZulipKeyValueStoreTest.kt
+++ android/app/src/test/java/com/zulipmobile/ZulipKeyValueStoreTest.kt
@@ -1,16 +1,24 @@
 package com.zulipmobile
 
+import android.app.Activity
+import android.app.Application
 import org.junit.Assert.*
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.Robolectric
 import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
+
+// This (and the `@Config(application = …)` using it) avoid an issue with Flipper:
+//   https://github.com/facebook/flipper/issues/1531#issuecomment-711436762
+class TestApplication: Application() {}
 
 @RunWith(RobolectricTestRunner::class)
+@Config(application = TestApplication::class)
 class ZulipKeyValueStoreTest {
     @Test
     fun `tmp test`() {
-        val controller = Robolectric.buildActivity(MainActivity::class.java)
+        val controller = Robolectric.buildActivity(Activity::class.java)
         controller.setup()
         val activity = controller.get()
 
