3245c1a8f1efcaba2876700bd6f63eb562324fdc
Greg Price <greg@zulip.com>
2022-12-07 16:59:22 -0800

    wip try reducing toward Android upstream example
    https://developer.android.com/training/testing/instrumented-tests
    
    But this version still not yet working:
    
    ~~~
    > Task :app:connectedDebugAndroidTest
    [InstrumentationResultParser]: invalid instrumentation status bundle unknown result
    Starting 0 tests on r-play-pixel3a(AVD) - 11
    Tests on r-play-pixel3a(AVD) - 11 failed: Instrumentation run failed due to 'Process crashed.'
    
    com.android.build.gradle.internal.testing.ConnectedDevice > No tests found.[r-play-pixel3a(AVD) - 11] FAILED
    No tests found. This usually means that your test classes are not in the form that your test runner expects (e.g. don't inherit from TestCase or lack @Test annotations).

--- android/app/src/androidTest/java/com/zulipmobile/StorageTest.kt
+++ android/app/src/androidTest/java/com/zulipmobile/StorageTest.kt
@@ -1,11 +1,19 @@
 package com.zulipmobile
 
-import android.test.AndroidTestCase
+//import android.test.AndroidTestCase
+import androidx.test.filters.SmallTest
+import androidx.test.runner.AndroidJUnit4
+import org.junit.Test
+import org.junit.runner.RunWith
+
 //import com.facebook.react.testing.ReactIntegrationTestCase;
 
 //class StorageTest: ReactIntegrationTestCase {
-class StorageTest : AndroidTestCase {
-    fun testSmoke() {
+@SmallTest
+@RunWith(AndroidJUnit4::class)
+class StorageTest {
+    @Test
+    fun smoke() {
         assert(false) { "hooray, reached test" }
     }
 }
